23ef49bc66b6d8b6807c332e75564a1d91251c8e
Sunny Chen <z5267282@student.unsw.edu.au>
2023-05-31 21:52:10 +1000

    test that indentation levels are not checked by python

---
+++ backend/dummy/bad-indentation.py
@@ -0,0 +1,16 @@
+def diff_levels():
+    i = 2
+    if i == 1:
+        print('one')
+    else:
+      print('two')
+
+def tabs_spaces():
+    i = 2
+    if i == 1:
+        print('one')
+    else:
+    	print('two')
+
+# diff_levels()
+tabs_spaces()
