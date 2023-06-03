6617491cea291e15b5014b510c43b4f3ced44f0f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-03 22:39:44 +1000

    manaully inserting code

--- backend/parser/main.py
+++ backend/parser/main.py
@@ -7,6 +7,15 @@ def trace_function(frame, event, arg):
     print(f"{str(frame.f_locals):<35} |")
     return trace_function
 
-with open("normal.py", "r") as f:
-    sys.settrace(trace_function)
-    exec(f.read())
+i = 1
+twos, sevens = 0, 0
+while i < 100:
+    if i % 2 == 0:
+        print("two")
+        twos += 1
+    elif i % 7 == 0:
+        print("seven")
+        sevens += 1
+    i += 1
+
+print(f"2s: {twos}, 7s: {sevens}")
