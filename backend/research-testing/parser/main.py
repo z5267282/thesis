44143ebb302ade7a5bdf540f38543193844b880a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-05 00:21:01 +1000

    experimenting with inspect to get what line number main starts on

--- backend/research-testing/parser/main.py
+++ backend/research-testing/parser/main.py
@@ -1,21 +1,36 @@
-import subprocess
+import inspect
+import linecache
 import sys
 
+filename = sys.argv[0]
+lines = []
+
+def main():
+    i = 1
+    twos, sevens = 0, 0
+    while i < 5:
+        if i % 2 == 0:
+            # print("two")
+            twos += 1
+        elif i % 7 == 0:
+            # print("seven")
+            sevens += 1
+        i += 1
+
+    # print(f"2s: {twos}, 7s: {sevens}")
+
 def trace_function(frame, event, arg):
-    print(f"| {event:10} | {str(arg):>4} |", end=' ')
-    print(f"{frame.f_lineno:>4} |", end=' ')
-    print(f"{str(frame.f_locals):<35} |")
+    if event == 'line':
+        lineno = frame.f_lineno
+        print(f'{lineno:2} : {linecache.getline(filename, lineno)[:-1]}')
+        lines.append(lineno)
     return trace_function
 
-i = 1
-twos, sevens = 0, 0
-while i < 100:
-    if i % 2 == 0:
-        print("two")
-        twos += 1
-    elif i % 7 == 0:
-        print("seven")
-        sevens += 1
-    i += 1
+def get_main_info():
+    lines, starting_lineno = inspect.getsourcelines(main)
+    print(starting_lineno)
 
-print(f"2s: {twos}, 7s: {sevens}")
+get_main_info()
+
+# sys.settrace(trace_function)
+# main()
