50a5e6da0e731a393661362301063b00ff2b93b3
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-05 02:44:01 +1000

    trying to add __name__ clause but trace still not running

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,21 +4,26 @@ import sys
 
 from program import program
 
-filename = sys.argv[0]
-lines = []
+
+def main():
+    filename = sys.argv[0]
+    lines = []
+
+    sys.settrace(trace_execution)
+    program()
 
 def trace_execution(frame, event, arg):
     if event == 'line':
         lineno = frame.f_lineno
-        line_contents = linecache.getline(filename, lineno)
-        print(f'{lineno:2} | {line_contents[:-1]}')
-        lines.append(lineno)
+        # line_contents = linecache.getline(filename, lineno)
+        # print(f'{lineno:2} | {line_contents[:-1]}')
+        # lines.append(lineno)
     return trace_execution
 
 def get_program_info():
     lines, starting_lineno = inspect.getsourcelines(program)
     print(starting_lineno)
-# get_program_info()
 
-sys.settrace(trace_execution)
-program()
+if __name__ == '__main__':
+    # get_program_info()
+    main()
