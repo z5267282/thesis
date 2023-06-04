592d4711619f7b5f4c0b6450826f953944e52f81
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-04 23:30:27 +1000

    playing around with inspect to get line contents

--- backend/line-counter.py
+++ backend/line-counter.py
@@ -1,32 +1,29 @@
+import inspect
 import sys
 
 lines = []
 
 def trace_function(frame, event, arg):
-    print(f"| {event:10} | {str(arg):>4} |", end=' ')
-    print(f"{frame.f_lineno:>4} |", end=' ')
-    print(f"{str(frame.f_locals):<35} |")
+    if event == 'line':
+        print(inspect.getmembers(frame))
+        lines.append(frame.f_lineno)
     return trace_function
 
-# def trace_function(frame, event, arg):
-#     print('here')
-#     if event == 'line':
-#         lines.append(frame.f_lineno)
-#     return trace_function
+def main():
+    i = 1
+    twos, sevens = 0, 0
+    while i < 5:
+        if i % 2 == 0:
+            print("two")
+            twos += 1
+        elif i % 7 == 0:
+            print("seven")
+            sevens += 1
+        i += 1
+
+    print(f"2s: {twos}, 7s: {sevens}")
+
+    print(lines)
 
 sys.settrace(trace_function)
-
-i = 1
-twos, sevens = 0, 0
-while i < 5:
-    if i % 2 == 0:
-        print("two")
-        twos += 1
-    elif i % 7 == 0:
-        print("seven")
-        sevens += 1
-    i += 1
-
-print(f"2s: {twos}, 7s: {sevens}")
-
-print(lines)
+main()
