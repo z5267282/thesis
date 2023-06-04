7e96dceef165209dc2b28db0c77646e80f27cbc9
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-04 23:53:30 +1000

    perhaps manual parsing for now will do

--- backend/research-testing/ast/parse.py
+++ backend/research-testing/ast/parse.py
@@ -3,4 +3,11 @@ import ast
 with open("normal.py", "r") as f:
     tree = ast.parse(f.read())
 
-print(ast.dump(tree, indent=2))
+class MyVisitor(ast.NodeVisitor):
+    def generic_visit(self, node):
+        print(f'Nodetype: {type(node).__name__:{16}} {node}')
+        ast.NodeVisitor.generic_visit(self, node)
+
+# print(ast.dump(tree, indent=2))
+visitor = MyVisitor()
+visitor.generic_visit(tree)
