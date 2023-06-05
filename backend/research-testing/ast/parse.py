b5e2644e0535bcaa39dc2689a8194863f7bc29a5
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-06 00:03:52 +1000

    not possible to get line numbers with the ast ;(

--- backend/research-testing/ast/parse.py
+++ backend/research-testing/ast/parse.py
@@ -1,13 +1,26 @@
 import ast
+import inspect
+import linecache
 
 with open("normal.py", "r") as f:
     tree = ast.parse(f.read())
 
 class MyVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.whiles = []
+
     def generic_visit(self, node):
-        print(f'Nodetype: {type(node).__name__:{16}} {node}')
+        if isinstance(node, ast.While):
+            self.whiles.append(node)
         ast.NodeVisitor.generic_visit(self, node)
 
 # print(ast.dump(tree, indent=2))
 visitor = MyVisitor()
 visitor.generic_visit(tree)
+
+for w in visitor.whiles:
+    # print(ast.dump(w, indent=2))
+    lines, number = inspect.getsourcelines(w)
+    print(type(w))
+    # for l in linecache.getlines("normal.py")[number : number + len(lines)]:
+    #     print(l[:-1])
