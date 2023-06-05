56fd307a868aa1da8ac1e6df2889ca42451f324a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-05 17:13:10 +1000

    printing out the wrong file

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,26 +4,21 @@ import sys
 
 from program import program
 
-
-def main():
-    filename = sys.argv[0]
-    lines = []
-
-    sys.settrace(trace_execution)
-    program()
+filename = inspect.getsourcefile(program)
+lines = []
 
 def trace_execution(frame, event, arg):
     if event == 'line':
         lineno = frame.f_lineno
-        # line_contents = linecache.getline(filename, lineno)
-        # print(f'{lineno:2} | {line_contents[:-1]}')
-        # lines.append(lineno)
+        line_contents = linecache.getline(filename, lineno)
+        print(f'{lineno:2} | {line_contents[:-1]}')
+        lines.append(lineno)
     return trace_execution
+
+sys.settrace(trace_execution)
 
 def get_program_info():
     lines, starting_lineno = inspect.getsourcelines(program)
     print(starting_lineno)
 
-if __name__ == '__main__':
-    # get_program_info()
-    main()
+program()
