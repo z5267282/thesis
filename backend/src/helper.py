5b2c8678a6340af06db4a023b8b2be10b529d5f4
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-06 00:46:48 +1000

    adding type hints to helper functions

--- backend/src/helper.py
+++ backend/src/helper.py
@@ -1,14 +1,17 @@
 import re
 
-def find_first_nospace(string):
+def find_first_nospace(string : str):
     '''a crude way of finding the first non-space character'''
     for i, s in enumerate(string):
         if not s.isspace():
             return i
     return len(string)
 
-def get_leading_whitespace(string):
+def get_leading_whitespace(string : str):
     return string[:find_first_nospace(string)]
+
+def num_leading_whitespace(string : str):
+    return len(get_leading_whitespace(string))
 
 # string = '    hello'
 # string = ''
