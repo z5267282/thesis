b5c36edbece85f603e20869c663f18881794e09f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-06 12:56:05 +1000

    adding temporary state tracker for current block type

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,23 +2,36 @@ import inspect
 import linecache
 import sys
 
+import helper
 from program import program
 
 filename = inspect.getsourcefile(program)
 lines = []
 
+class State:
+    def __init__(self):
+        self.indent_level = None
+
+state = State()
+
 def trace_execution(frame, event, arg):
-    if event == 'line':
-        lineno = frame.f_lineno
-        line_contents = linecache.getline(filename, lineno)
-        print(f'{lineno:2} | {line_contents[:-1]}')
-        lines.append(lineno)
+    # only consider normal lines for now
+    if event != "line":
+        return trace_execution
+    
+    line_no = frame.f_lineno
+    line_contents = linecache.getline(filename, line_no)
+    leading_space = helper.num_leading_whitespace(line_contents)
+
+    # first line
+    if state.indent_level is None:
+        state.indent_level = leading_space
+
+    print(f'{line_no:2} | {line_contents[:-1]}')
+    lines.append(line_no)
+
     return trace_execution
 
 sys.settrace(trace_execution)
-
-def get_program_info():
-    lines, starting_lineno = inspect.getsourcelines(program)
-    print(starting_lineno)
 
 program()
