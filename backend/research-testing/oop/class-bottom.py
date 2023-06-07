f5eee3b98dd96daab43b42002d6546e2846ab837
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-07 23:37:57 +1000

    composed classes can be added in any order

---
+++ backend/research-testing/oop/class-bottom.py
@@ -0,0 +1,9 @@
+class Main:
+    def __init__(self):
+        self.c = ComposedMember()
+
+class ComposedMember:
+    pass
+
+m = Main()
+print(m)
---
+++ backend/research-testing/oop/class-top.py
@@ -0,0 +1,9 @@
+class ComposedMember:
+    pass
+
+class Main:
+    def __init__(self):
+        self.c = ComposedMember()
+
+m = Main()
+print(m)
