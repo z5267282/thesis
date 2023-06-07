8d9beb92bee7e5cdcecadf75f2a71a9dacc13842
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-08 01:06:28 +1000

    rewriting classes to use custom constructor

--- backend/src/errors.py
+++ backend/src/errors.py
@@ -1,11 +1,11 @@
 class ElifParseError(Exception):
-    def __str__(self):
-        return "an elif block was parsed without a same level if first"
+    def __init__(self):
+        super().__init__("an elif block was parsed without a same level if first")
 
 class ElseParseError(Exception):
-    def __str__(self):
-        return "an else block was parsed without a same level if first"
+    def __init__(self):
+        super().__init__("an else block was parsed without a same level if first")
 
 class UnsupportedIndentation(Exception):
-    def __str__(self):
-        return "an indentation involving an unsupported code feature was wirtten"
+    def __intit__(self):
+        super().__init__("an indentation involving an unsupported code feature was wirtten")
