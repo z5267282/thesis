6d9fe806a96bb309291a4d8ee77878c8e36121f1
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-07 14:51:11 +1000

    first draft of the tree structure

--- backend/src/tree.py
+++ backend/src/tree.py
@@ -1,8 +1,24 @@
-class Block():
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+import typing
 
-class AssignmentBlock(Block):
-    def __init__(self, start, end):
-        super.__init__(start, end)
+class Block():
+    def __init__(self, start : int):
+        self.start : int = start
+        self.end   : int = None
+
+class CodeBlock(Block):
+    pass
+
+class BodyBlock(Block):
+    def __init__(self, start: int):
+        super().__init__(start)
+        self.body : typing.List[Block] = []
+
+class WhileBlock(BodyBlock):
+    pass
+
+class IfBlock(BodyBlock):
+    def __init__(self, start: int):
+        super().__init__(start)
+        self.elifs : typing.List[IfBlock] = []
+        # must call it this because else is a reserved word
+        self.else_ : None | BodyBlock = None
