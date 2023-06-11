96cd573d1f3ce061b75633fe0d6debfd3415a13d
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-11 18:47:07 +1000

    isolated parsing seems to work

--- backend/src/tree.py
+++ backend/src/tree.py
@@ -1,4 +1,3 @@
-import json
 from typing import List, Type, Union
 
 class Block():
@@ -6,7 +5,7 @@ class Block():
         self.start    : int = start
         self.end      : int = None
 
-    def __dict__(self):
+    def to_dict(self):
         return {
             self.__class__.__name__ : {
                 "start" : self.start,
@@ -18,15 +17,20 @@ class CodeBlock(Block):
     pass
 
 # putting this here because it apparently can't be a class variable
+# pipe syntax does not work for forward references so use Union
 ForwardReferenceOptionalBody = Union['BodyBlock', CodeBlock]
 class BodyBlock(Block):
     def __init__(self, start: int):
         super().__init__(start)
         self.body : List[ForwardReferenceOptionalBody] = []
     
-    # pipe syntax does not work for forward references so use Union
     def add_same_level_block(self, block : ForwardReferenceOptionalBody):
         self.body.append(block)
+    
+    def to_dict(self):
+        parent = super().to_dict()
+        parent[self.__class__.__name__]["body"] = [b.to_dict() for b in self.body]
+        return parent
     
 class WhileBlock(BodyBlock):
     pass
--- backend/tests/test_parsing.py
+++ backend/tests/test_parsing.py
@@ -3,13 +3,9 @@ from src.tree import BodyBlock, CodeBlock
 def test_code_block():
     c = CodeBlock(1)
     c.end = 10
-    print(dict(c))
-    # assert dict(c) == {
-    #     "CodeBlock" : {
-    #         "start" : 1,
-    #         "end"   : 10
-    #     }
-    # }
+    assert c.to_dict() == {
+        "CodeBlock" : {"start" : 1,"end" : 10}
+    }
 
 def test_body_block():
     b = BodyBlock(1)
@@ -23,4 +19,13 @@ def test_body_block():
     c2.end = 10
     b.add_same_level_block(c2)
 
-    assert True
+    assert b.to_dict() == {
+        "BodyBlock": {"start": 1, "end"  : 10, "body" : [
+                {"CodeBlock": {"start": 1, "end": 4}},
+                {"CodeBlock": {"start": 5, "end": 10}}
+            ]
+        }
+    }
+
+def test_if_block():
+
