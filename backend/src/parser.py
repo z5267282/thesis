e3b48e568f38ed8f6ab568af22a3d1e0847198db
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-12 01:29:04 +1000

    major work on parsing

--- backend/src/parser.py
+++ backend/src/parser.py
@@ -5,101 +5,108 @@ from typing import Callable, Deque, Type
 from errors import ElseParseError, ElifParseError
 import helper
 from tree import \
-    CodeBlock, BodyBlock, BodyBlockDescendant, OptionalBodyBlock, \
+    Block, CodeBlock, BodyBlock, OptionalBodyBlock, \
     IfBlock, ElseBlock, ElifBlock, ConditionalBlock, WhileBlock
 
 def parse(program : Callable):
     lines, start = inspect.getsourcelines(program)
     # assume the first line is the function definition
     start += 1
-    root  : BodyBlock = BodyBlock(start)
-    state : State = State(root)
+    root        : BodyBlock = None
+    stack       : Stack = None
+    prev_indent : int = None
+    line_no     : int = 0
     for line_no, line_contents in enumerate(lines, start=start):
-        line          : str = helper.get_stripped_line(line_contents)
-        leading_space : int = helper.num_leading_whitespace(line_contents)
-        top           : BodyBlockDescendant = state.stack.peek()
-        # use indent_level to track whether the first line has been entered
-        if state.indent_level is None:
-            state.indent_level = leading_space
-            first_block = parse_line(line, line_no)
-            top.add_same_level_block(first_block)
+        line        : str = helper.get_stripped_line(line_contents)
+        indent_level : int = helper.num_leading_whitespace(line_contents)
+
+        # first line
+        if prev_indent is None:
+            prev_indent = indent_level
+            first_block : Type[Block] = parse_line(line, line_no, indent_level)
             if isinstance(first_block, ElifBlock):
                 raise ElifParseError
             if isinstance(first_block, ElseBlock):
                 raise ElseParseError
-            if isinstance(first_block, CodeBlock):
-                # assign the end through the state
-                state.stack.code_block = first_block
-            else:
-                state.stack.push(first_block)
-        # found indented block
-        # the current BodyBlock should not have ended
-        if leading_space > state.indent_level:
-            nested_block = parse_line(line, line_no)
-            if isinstance(nested_block, CodeBlock):
-                state.code_block = nested_block 
-            else:
-                top.add_same_level_block(nested_block)
-                state.stack.push(nested_block) 
-        # unindented block
-        # an indented block has just ended
-        elif leading_space < state.indent_level:
-            prev : int = line_no - 1
-            if state.code_block is not None:
-                state.code_block.end = prev
-                top.add_same_level_block(state.code_block)
-                state.code_block = None
-            top.end = prev
-            state.stack.pop()
-            unnested_block = parse_line(line, line_no)
-            top = state.stack.peek()
-            if isinstance(nested_block, CodeBlock):
-                state.code_block = nested_block 
-            else:
-                top.add_same_level_block(unnested_block)
-                state.stack.push(unnested_block) 
-        # same level block
-        else:
-            # code block
-            next_block = parse_line(line, line_no)
-            if not isinstance(next_block, CodeBlock):
-                prev : int = line_no - 1
-                if state.code_block is not None:
-                    state.code_block.end = prev
-                    top.add_same_level_block(state.code_block)
-                    state.code_block = None
-                else:
-                    # TODO
-                    pass
 
-            # or nested block
-        state.indent_level = leading_space
+            root = BodyBlock(line_no, line_contents)
+            stack = Stack(root)
+            if isinstance(first_block, (IfBlock, WhileBlock)):
+                stack.push(first_block)
+            elif isinstance(first_block, CodeBlock):
+                root.code_block = first_block
+            root.add_same_level_block(first_block)
+            continue
+
+        top  : Type[BodyBlock] = stack.peek()
+        if indent_level == prev_indent:
+            level_block : Type[Block] = parse_line(line, line_no, indent_level)
+            if isinstance(level_block, CodeBlock):
+                # no need to set the indentation level if its the same
+                continue
+            if isinstance(level_block, ElifBlock):
+                raise ElifParseError
+            if isinstance(level_block, ElseBlock):
+                raise ElseParseError
+
+            if isinstance(level_block, (IfBlock, WhileBlock)):
+                if top.code_block is not None:
+                    prev : int = line_no - 1
+                    top.code_block.end = prev
+                    top.code_block = None
+                top.add_same_level_block(level_block)
+                stack.push(level_block)
+
+        # indented block
+        elif indent_level > prev_indent:
+            nested_block : Type[Block] = parse_line(line, line_no, indent_level)
+            if isinstance(first_block, ElifBlock):
+                raise ElifParseError
+            if isinstance(first_block, ElseBlock):
+                raise ElseParseError
+
+            if isinstance(nested_block, CodeBlock):
+                top.code_block = nested_block 
+            elif isinstance(first_block, (IfBlock, WhileBlock)):
+                stack.push(nested_block) 
+            top.add_same_level_block(nested_block)
+        # unindented block
+        elif indent_level < prev_indent:
+            prev : int = line_no - 1
+            if top.code_block is not None:
+                top.code_block.end = prev
+                top.code_block = None
+            # pop off stack until same level block is found
+            while top.indent_level != indent_level:
+                top.end = prev
+                stack.pop()
+                top = stack.peek()
+            unnested_block = parse_line(line, line_no, indent_level)
+            if isinstance(unnested_block, CodeBlock):
+                top.code_block = unnested_block 
+            if isinstance(unnested_block, ElifBlock):
+                # if 
+                pass
+            top.add_same_level_block(unnested_block)
+
+        prev_indent = indent_level
     # TODO: last line
 
-def parse_line(line : str, line_no : int):
+def parse_line(line : str, line_no : int, indent_level : int):
     if line.startswith("if"):
-        return IfBlock(line_no)
+        return IfBlock(line_no, indent_level)
     if line.startswith("while"):
-        return WhileBlock(line_no)
+        return WhileBlock(line_no, indent_level)
     if line.startswith("elif"):
-        return ElifBlock(line_no)
+        return ElifBlock(line_no, indent_level)
     if line.startswith("else"):
-        return ElseBlock(line_no)
+        return ElseBlock(line_no, indent_level)
     return CodeBlock(line_no)
-
-class State:
-    """a dataclass to store any necessary information during parsing
-    used as the function to sys.settrace can only take in one helper argument"""
-    def __init__(self, root : BodyBlock):
-        self.indent_level : int = None
-        # the current code block if any
-        # these should never be on the stack
-        self.stack        : Stack = Stack(root)
 
 class Stack:
     """a simple wrapper around deque"""
     def __init__(self, root : BodyBlock):
-        self.items : Deque[BodyBlockDescendant] = deque()
+        self.items : Deque[Type[BodyBlock]] = deque()
         self.push(root)
     
     def __len__(self):
@@ -115,10 +122,3 @@ class Stack:
         """method according to the documentation here:
         https://docs.python.org/3/library/collections.html#collections.deque"""
         return self.items[-1]
-
-def init_root(program : Callable):
-    # for now assume program is only top level code
-    lines, start = inspect.getsourcelines(program)
-    root = BodyBlock(start + 1)
-    root.end = len(lines)
-    return root
