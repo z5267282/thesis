eab2d6744e731d1e194d4a39296bbd08b7fb4e94
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-15 03:17:10 +1000

    starting work on frontend

---
+++ frontend/src/components/CodeBox.jsx
@@ -0,0 +1,11 @@
+function CodeBox() {
+  const style = {
+    marginTop : "100px"
+  };
+  return (
+    <textarea rows="40" cols="80" id="code-upload" name="fish" style={style}>
+    </textarea>
+  );
+}
+
+export default CodeBox;
