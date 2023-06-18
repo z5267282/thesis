7a7cd5999485c820a5170630ac5725ac5f917a51
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-19 02:14:59 +1000

    code upload box done now need to port to css

--- frontend/src/components/CodeBox.jsx
+++ frontend/src/components/CodeBox.jsx
@@ -1,16 +1,58 @@
-function CodeBox() {
-  const update = (event) => {console.log("hello!")};
-  const style = {
-    marginTop : "75"
+export default function CodeBox() {
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   };
+
+  const headerStyle = {
+    fontSize: "16pt"
+  };
+
+  const textStyle = {
+    resize: "none",
+    padding: "1%",
+    fontSize: "12pt",
+    border: "solid",
+    borderWidth: "1.5pt",
+    borderRadius: "1%"
+  };
+  const update = (event) => {console.log(event.target.value)};
+
+  const buttonBoxStyle = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    width: "20%",
+    marginTop: "2%"
+  }
+
+  const buttonStyle = {
+    fontSize: "14pt",
+    border: "solid",
+    borderWidth: "1.5pt",
+    borderRadius: "5pt",
+  }
+
   return (
-    <form id="code-upload">
+    <form
+      id="code-upload"
+      style={formStyle}
+    >
+      <p style={headerStyle}>Code Input</p>
       <textarea
-        rows="40" cols="80" name="code-upload" style={style} defaultValue="" onChange={update}
-      >
-      </textarea>
+        rows={30}
+        cols={60}
+        name="code-upload"
+        style={textStyle}
+        defaultValue=""
+        onChange={update}
+        spellCheck={false}
+        autoFocus={false}
+      />
+      <div style={buttonBoxStyle}>
+        <button style={buttonStyle}>prev</button>
+        <button style={buttonStyle}>next</button>
+      </div>
     </form>
   );
 }
-
-export default CodeBox;
