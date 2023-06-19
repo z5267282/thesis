75dda0d972f2ed703df7d0316c388216bc74e28a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-19 18:28:45 +1000

    forgot to add file to index

---
+++ backend/tests/tree_parser_parse/test_nested_if.py
@@ -0,0 +1,51 @@
+from tree_parser import parse
+
+def test_nested_if():
+    def nested():
+        i = 6
+        if i % 2 == 0:
+            if i % 3 == 0:
+                print("multiple of 6")
+
+    root = parse(nested)
+    print(root.pretty_print())
+    assert root.to_dict() == {
+        "BodyBlock" : {
+            "start" : 5,
+            "end" : 8,
+            "body" : [
+                {
+                    "CodeBlock" : {
+                        "start" : 5,
+                        "end" : 5
+                    }
+                },
+                {
+                    "IfBlock" : {
+                        "start" : 6,
+                        "end" : 8,
+                        "elifs" : [],
+                        "else" : None,
+                        "body" : [
+                            {
+                                "IfBlock" : {
+                                    "start" : 7,
+                                    "end" : 8,
+                                    "elifs" : [],
+                                    "else" : None,
+                                    "body" : [
+                                        {
+                                            "CodeBlock" : {
+                                                "start" : 8,
+                                                "end" : 8
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
