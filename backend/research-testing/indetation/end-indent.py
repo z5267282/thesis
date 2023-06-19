861395bc99fc671b0d5526ade7cb3f72a1798e52
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-19 21:32:01 +1000

    empty lines are okay

---
+++ backend/research-testing/indetation/end-indent.py
@@ -0,0 +1,17 @@
+# def blank_line():
+#     i = 0
+#     while i < 13:
+#         i += 1
+
+#      print('x')
+
+def filled_line():
+    """there are spaces in the empty line"""
+    i = 0
+    while i < 13:
+        i += 1
+   
+    print('x')
+
+# blank_line()
+filled_line()
