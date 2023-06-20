305f47d8f282e6bcac4d8ff0667728ebc54ff9ab
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-20 16:47:19 +1000

    if elif teting

---
+++ backend/tests/tree_parser_parse/test_if_elif.py
@@ -0,0 +1,54 @@
+from tree_parser import parse
+
+def test_if_elif():
+    def if_elif():
+        i = 0
+        if i == 1:
+            print("one")
+        elif i == 2:
+            print("two")
+    
+    root = parse(if_elif)
+    assert root.to_dict() == {
+        "BodyBlock" : {
+            "start" : 5,
+            "end" : 9,
+            "body" : [
+                {
+                    "CodeBlock" : {
+                        "start" : 5,
+                        "end" : 5
+                    }
+                },
+                {
+                    "IfBlock" : {
+                        "start" : 6,
+                        "end" : 9,
+                        "body" : [
+                            {
+                                "CodeBlock" : {
+                                    "start" : 7,
+                                    "end" : 7
+                                }
+                            }
+                        ],
+                        "elifs" : [
+                            {
+                                "ElifBlock" : {
+                                    "start" : 8,
+                                    "end" : 9,
+                                    "body" : [
+                                        "CodeBlock" : {
+                                            "start" ; 9,
+                                            "end" : 9
+                                        }
+                                    ]
+                                }
+                            }
+                        ],
+                        "else" : None
+                    }
+                }
+            ]
+        }
+    }
