ec42ae0e933a9e0afa3e43d631363873305bd5a9
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-21 21:49:32 +1000

    forgetting to add test file

---
+++ backend/tests/tree_parser_parse/test_if_elif_chain.py
@@ -0,0 +1,79 @@
+from tree_parser import parse
+
+def test_if_elif_chain():
+    def if_elif_chain():
+        i = 0
+        if i == 1:
+            print("one")
+        elif i == 2:
+            print("two")
+        elif i == 3:
+            print("three")
+        print(i)
+    
+    root = parse(if_elif_chain)
+    assert root.to_dict() == {
+        "BodyBlock" : {
+            "start" : 5,
+            "end" : 12,
+            "body" : [
+                {
+                    "CodeBlock" : {
+                        "start" : 5,
+                        "end" : 5
+                    }
+                },
+                {
+                    "IfBlock" : {
+                        "start" : 6,
+                        "end" : 11,
+                        "body" : [
+                            {
+                                "CodeBlock" : {
+                                    "start" : 7,
+                                    "end" : 7
+                                }
+                            }
+                        ],
+                        "elifs" : [
+                            {
+                                "ElifBlock" : {
+                                    "start" : 8,
+                                    "end" : 9,
+                                    "body" : [
+                                        {
+                                            "CodeBlock" : {
+                                                "start" : 9,
+                                                "end" : 9
+                                            }
+                                        }
+                                    ]
+                                }
+                            },
+                            {
+                                "ElifBlock" : {
+                                    "start" : 10,
+                                    "end" : 11,
+                                    "body" : [
+                                        {
+                                            "CodeBlock" : {
+                                                "start" : 11,
+                                                "end" : 11
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ],
+                        "else" : None
+                    }
+                },
+                {
+                    "CodeBlock" : {
+                        "start" : 12,
+                        "end" : 12
+                    }
+                }
+            ]
+        }
+    }
