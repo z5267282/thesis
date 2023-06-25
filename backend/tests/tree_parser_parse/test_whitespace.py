380398d7269ef91e4fb758adb6569725330b704e
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-26 01:32:19 +1000

    comment and whitespace test

---
+++ backend/tests/tree_parser_parse/test_whitespace.py
@@ -0,0 +1,71 @@
+from tree_parser import parse
+
+def test_whitespace():
+    def whitespace():
+        # this is a header comment
+        i = 0
+        while i < 10:
+            # print every even number
+            if i % 2 == 0:
+                print(i)
+            i += 1
+        
+        print(f"the last value of i is {i}")
+    
+    root = parse(whitespace)
+    assert root.to_dict() == {
+        "BodyBlock" : {
+            "start" : 5,
+            "end" : 13,
+            "body" : [
+                {
+                    "CodeBlock" : {
+                        "start" : 6,
+                        "end" : 6
+                    }
+                },
+                {
+                    "WhileBlock" : {
+                        "start" : 7,
+                        "end" : 12,
+                        "body" : [
+                            {
+                                "CodeBlock" : {
+                                    "start" : 8,
+                                    "end" : 8
+                                }
+                            },
+                            {
+                                "IfBlock" : {
+                                    "start" : 9,
+                                    "end" : 10,
+                                    "body" : [
+                                        {
+                                            "CodeBlock" : {
+                                                "start" : 10,
+                                                "end" : 10
+                                            }
+                                        }
+                                    ],
+                                    "elifs" : [],
+                                    "else" : None
+                                }
+                            },
+                            {
+                                "CodeBlock" : {
+                                    "start" : 11,
+                                    "end" : 12
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    "CodeBlock" : {
+                        "start" : 13,
+                        "end" : 13
+                    }
+                }
+            ]
+        }
+    }
