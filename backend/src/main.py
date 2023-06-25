482ec9c15f3d446d3753e31b3bb6f641226e418a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-26 03:06:17 +1000

    need to run the function

--- backend/src/main.py
+++ backend/src/main.py
@@ -19,9 +19,29 @@ def handler(frame : FrameType, event : str, arg : Any, lines, vars):
 
 def trace_hook_arg(handler, lines, vars):
     def wrapper(frame : FrameType, event : str, arg : Any):
-        return handler(frame, event, arg, lines, vars)
+        handler(frame, event, arg, lines, vars)
+        return wrapper
     
     sys.settrace(wrapper)
 
 if __name__ == '__main__':
     main()
+
+# def main():
+#     lines = []
+#     settrace_closure(trace_execution, lines)
+#     program()
+#     print(lines)
+    
+# def trace_execution(frame, event, arg, lines):
+#     lines.append(frame.f_lineno)
+
+# def settrace_closure(trace_execution, lines):
+#     def wrapper(frame, event, arg):
+#         trace_execution(frame, event, arg, lines)
+#         return wrapper
+    
+#     sys.settrace(wrapper)
+
+# if __name__ == '__main__':
+#     main()
