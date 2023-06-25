6dd906b07015baeb6d1ec39e8049ae3c68d6db50
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-25 22:44:20 +1000

    else testing

---
+++ backend/tests/tree_parser_parse/test_if_else.py
@@ -0,0 +1,55 @@
+from tree_parser import parse
+
+def test_if_else():
+    def if_else():
+        i = 0
+        if i == 1:
+            print("one")
+        else:
+            print("something else!")
+    
+    root = parse(if_else)
+    root.pretty_print()
+    assert root.to_dict() == {
+        "BodyBlock" : {
+            "start" : 5,
+            "end" : 9,
+            "body" : [
+                {
+                    "CodeBlock" : {
+                        "start" : 5,
+                        "end" : 5
+                    }
+                },
+                {
+                    "IfBlock" : {
+                        "start" : 6,
+                        "end" : 9,
+                        "body" : [
+                            {
+                                "CodeBlock" : {
+                                    "start" : 7,
+                                    "end" : 7
+                                }
+                            }
+                        ],
+                        "elifs" : [],
+                        "else" : {
+                            "ElseBlock" : {
+                                "start" : 8,
+                                "end" : 9,
+                                "body" : [
+                                    {
+                                        "CodeBlock" : {
+                                            "start" : 9,
+                                            "end" : 9
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }
