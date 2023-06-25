10021e2b06c676cc2217954ea7b94d2ac518af84
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-26 03:10:48 +1000

    should work now

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,21 +5,25 @@ from typing import Any, List
 from tree_parser import parse
 from program import program
 
+class Line:
+    """dataclass to store line information"""
+    def __init__(self, line_no : int, locals : dict):
+        self.line_no : int  = line_no
+        self.locals  : dict = locals
+
 def main():
-    lines, vars = [], []
-    trace_hook_arg(handler, lines, vars)
+    lines = []
+    trace_hook_arg(handler, lines)
     program()
-    print(lines)
-    print(vars)
+    for l in lines:
+        print(l.line_no)
 
-def handler(frame : FrameType, event : str, arg : Any, lines, vars):
-    print(frame.f_lineno)
-    lines.append(frame.f_lineno)
-    vars.append(frame.f_locals)
+def handler(frame : FrameType, event : str, arg : Any, lines):
+    lines.append(Line(frame.f_lineno, frame.f_locals))
 
-def trace_hook_arg(handler, lines, vars):
+def trace_hook_arg(handler, lines):
     def wrapper(frame : FrameType, event : str, arg : Any):
-        handler(frame, event, arg, lines, vars)
+        handler(frame, event, arg, lines)
         return wrapper
     
     sys.settrace(wrapper)
