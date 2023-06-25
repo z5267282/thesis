2097e6d2056933e17c17772ac50524df86efd53b
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-25 23:22:32 +1000

    adding helper get whitespace tests

--- backend/tests/test_helper.py
+++ backend/tests/test_helper.py
@@ -3,3 +3,9 @@ import helper
 def test_get_stripped_line_all_whitespace():
     whitespace = "     "
     assert helper.get_stripped_line(whitespace) == ""
+
+def test_get_leading_whitespace():
+    # 4 space bars
+    string = '    hello'
+    space = helper.get_leading_whitespace(string)
+    assert space == '    '
