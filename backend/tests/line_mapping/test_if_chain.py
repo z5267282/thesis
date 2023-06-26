cf760f3635c07a20f083e0ba76523b7d1d040546
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-26 21:53:29 +1000

    if chain failing

---
+++ backend/tests/line_mapping/test_if_chain.py
@@ -0,0 +1,47 @@
+from tree import BodyBlock, CodeBlock, ElifBlock, ElseBlock, IfBlock
+
+def test_if_chain():
+    """templated from: 
+    i = 0
+    if i == 1:
+        ...
+    elif i == 2:
+        ...
+    else:
+        ..."""
+    c_start = CodeBlock(5)
+    c_start.end = 5
+
+    i = IfBlock(6, 1)
+    i.end = 11
+    i_body = CodeBlock(7)
+    i_body.end = 7
+
+    el = ElifBlock(8, 1)
+    el.end = 9
+    el_body = CodeBlock(9)
+    el_body.end = 9
+    i.add_elif(el)
+
+    els = ElseBlock(10, 1)
+    els.end = 11
+    els_body = CodeBlock(11)
+    els_body.end = 11
+    i.add_else(els)
+
+    root = BodyBlock(5, 1)
+    root.end = 11
+    root.add_same_level_block(c_start)
+    root.add_same_level_block(i)
+
+    line_mappings = {}
+    root.map_lines(line_mappings)
+    assert line_mappings == {
+        5 : c_start,
+        6 : i,
+        7 : i_body,
+        8 : el,
+        9 : el_body,
+        10 : els,
+        11 : els_body
+    }
