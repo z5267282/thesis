e0b2799c62abc0860944f6bd551ef7a589c8a1de
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-28 14:07:27 +1000

    simple display works

--- frontend/src/components/CodeBox.jsx
+++ frontend/src/components/CodeBox.jsx
@@ -2,41 +2,54 @@ import './style.css';
 import React from 'react';
 
 export default function CodeBox() {
-  const formStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "65%"
-  };
-
-  const update = (event) => {console.log(event.target.value)};
-
-  const buttonBoxStyle = {
-    display: "flex",
-    justifyContent: "space-evenly",
-    width: "50%",
-    marginTop: "2%"
+  const [text, setText] = React.useState("");
+  const displayFromCSV = (csvString) => {
+    const csvList = csvString.split(", ");
+    setText(csvList.join("\n"));
   }
-
+  
   return (
-    <form
-      id="code-upload"
-      style={formStyle}
-    >
-      <p className="large-text">Code Input</p>
-      <textarea
-        defaultValue="hello"
-        rows={30}
-        cols={60}
-        name="code-upload"
-        onChange={update}
-        spellCheck={false}
-        autoFocus={false}
-      />
-      <div style={buttonBoxStyle}>
-        <button>prev</button>
-        <button>next</button>
-      </div>
-    </form>
+    <>
+      <textarea id="display" disabled={true} value={text}></textarea>
+      <textarea id="enter" onInput={(event) => {displayFromCSV(event.target.value)}}></textarea>
+    </>
   );
+
+  // const formStyle = {
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   alignItems: "center",
+  //   width: "65%"
+  // };
+
+  // const update = (event) => {console.log(event.target.value)};
+
+  // const buttonBoxStyle = {
+  //   display: "flex",
+  //   justifyContent: "space-evenly",
+  //   width: "50%",
+  //   marginTop: "2%"
+  // }
+
+  // return (
+  //   <form
+  //     id="code-upload"
+  //     style={formStyle}
+  //   >
+  //     <p className="large-text">Code Input</p>
+  //     <textarea
+  //       defaultValue="hello"
+  //       rows={30}
+  //       cols={60}
+  //       name="code-upload"
+  //       onChange={update}
+  //       spellCheck={false}
+  //       autoFocus={false}
+  //     />
+  //     <div style={buttonBoxStyle}>
+  //       <button>prev</button>
+  //       <button>next</button>
+  //     </div>
+  //   </form>
+  // );
 }
