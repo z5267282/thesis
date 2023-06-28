7c9411c55482a2e00e29f50593cc7ec88b9e54d6
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-28 15:02:06 +1000

    not sure why box is not showing

--- frontend/src/components/CodeBox.jsx
+++ frontend/src/components/CodeBox.jsx
@@ -2,54 +2,52 @@ import './style.css';
 import React from 'react';
 
 export default function CodeBox() {
-  const [text, setText] = React.useState("");
-  const displayFromCSV = (csvString) => {
-    const csvList = csvString.split(", ");
-    setText(csvList.join("\n"));
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "65%"
+  };
+
+  const buttonBoxStyle = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    width: "50%",
+    marginTop: "2%"
   }
-  
+
+  const CODE_KEY = "code";
+  const [showTrace, setShowTrace] = React.useState(true);
+  console.log(showTrace)
+
   return (
-    <>
-      <textarea id="display" disabled={true} value={text}></textarea>
-      <textarea id="enter" onInput={(event) => {displayFromCSV(event.target.value)}}></textarea>
-    </>
+    <form
+      id="code-upload"
+      style={formStyle}
+    >
+      <p className="large-text">Code Input</p>
+      <div style={{ display : "flex", width : "20%", justifyContent : "space-evenly", marginBottom : 20 }}>
+        <button onClick={() => {setShowTrace(true)}}>Trace</button>
+        <button onClick={() => {setShowTrace(false)}}>Upload</button>
+        {/* <button onClick={() => {if (!showTrace) setShowTrace(true)}}>Trace</button>
+        <button onClick={() => {if (showTrace) setShowTrace(false)}}>Upload</button> */}
+      </div>
+      {
+        (showTrace) ?
+          <textarea
+            rows={30} cols={60} name="code-upload" spellCheck={false} disabled
+            value={localStorage.hasOwnProperty(CODE_KEY) ? localStorage.getItem(CODE_KEY) : "[no code]"}
+          />
+        :
+        <textarea
+          defaultValue="enter code here" rows={30} cols={60} name="code-upload" spellCheck={false}
+          onInput={() => localStorage.setItem(CODE_KEY)} 
+        />
+      }
+      <div style={buttonBoxStyle}>
+        <button>prev</button>
+        <button>next</button>
+      </div>
+    </form>
   );
-
-  // const formStyle = {
-  //   display: "flex",
-  //   flexDirection: "column",
-  //   alignItems: "center",
-  //   width: "65%"
-  // };
-
-  // const update = (event) => {console.log(event.target.value)};
-
-  // const buttonBoxStyle = {
-  //   display: "flex",
-  //   justifyContent: "space-evenly",
-  //   width: "50%",
-  //   marginTop: "2%"
-  // }
-
-  // return (
-  //   <form
-  //     id="code-upload"
-  //     style={formStyle}
-  //   >
-  //     <p className="large-text">Code Input</p>
-  //     <textarea
-  //       defaultValue="hello"
-  //       rows={30}
-  //       cols={60}
-  //       name="code-upload"
-  //       onChange={update}
-  //       spellCheck={false}
-  //       autoFocus={false}
-  //     />
-  //     <div style={buttonBoxStyle}>
-  //       <button>prev</button>
-  //       <button>next</button>
-  //     </div>
-  //   </form>
-  // );
 }
