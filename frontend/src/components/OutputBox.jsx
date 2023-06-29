d7e15edc6fc207eef3659f714a0db72426f9f294
Sunny Chen <z5267282@student.unsw.edu.au>
2023-06-29 16:32:00 +1000

    output box

--- frontend/src/components/OutputBox.jsx
+++ frontend/src/components/OutputBox.jsx
@@ -1,25 +1,11 @@
 import styles from "./OutputBox.module.css";
 
 export default function VariableBox() {
-  const style = {
-    width: "100%",
-    height: "30%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  };
 
   return (
-    <div
-      style={style}
-    >
+    <label htmlFor="outputBox" className={styles.container}>
       <p className="large-text">Output</p>
-      <textarea
-        value="out"
-        className={styles.sideBox}
-        spellCheck={false}
-        readOnly={true}
-      />
-    </div>
+      <textarea id="outputBox" value="out" spellCheck={false} readOnly/>
+    </label>
   );
 };
