53ecea5cccdeeb545de3e10d55401cc3be81f158
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-01 23:13:35 +1000

    will need to move codebox into separate file

--- frontend/src/components/CodeBox.jsx
+++ frontend/src/components/CodeBox.jsx
@@ -6,6 +6,31 @@ export default function CodeBox() {
   const [showTrace, setShowTrace] = React.useState(false);
   const [traceCode, setTraceCode] = React.useState("");
 
+  const TraceBox = (
+    <textarea
+      rows={30} cols={60} spellCheck={false} disabled value={traceCode} id="box"
+    />
+  );
+
+  const UploadBox = (
+    <>
+      <div className={styles.hello}>
+        {
+          traceCode.split("\n").map((_, line) => {
+            console.log("hello");
+            return (
+              <p key={`line-${line}`}>{line + 1}</p>
+            );
+          })
+        }
+      </div>
+      <textarea
+        rows={30} cols={60} name="code-upload" spellCheck={false} id="box"
+        onInput={(event) => setTraceCode(event.target.value)} 
+      />
+    </>
+  );
+
   return (
     <div className={styles.container}>
       <div className={styles.tabSelector}>
@@ -16,17 +41,7 @@ export default function CodeBox() {
         <p className={styles.largeText}>
           { (showTrace) ? "Trace execution" : "Upload code" }
         </p>
-        {
-          (showTrace) ?
-            <textarea
-              rows={30} cols={60} name="code-upload" spellCheck={false} disabled value={traceCode} id="box"
-            />
-          :
-          <textarea
-            rows={30} cols={60} name="code-upload" spellCheck={false} id="box"
-            onInput={(event) => setTraceCode(event.target.value)} 
-          />
-        }
+        { (showTrace) ? TraceBox : UploadBox }
       </label>
       <div className={styles.transitionContainer}>
         <button>prev</button>
