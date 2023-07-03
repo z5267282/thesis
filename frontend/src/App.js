cda5800c3db8bd158a63e6eb56eb69e6ea940e98
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-03 17:18:57 +1000

    ready to align arrows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,10 @@ export default function App() {
           <OutputBox />
         </div>
       </div>
+
+      <svg width={300} height={300}>
+        <path d="M 0 0 Q 50 50 0 100" stroke="black" fill="transparent" />
+      </svg>
     </div>
   );
 }
