c53cfdeede8d34b834b3d28b55bd34f271d26f3f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-05 17:28:00 +1000

    cannot use useRef inside useEffect so alnterante solution

--- frontend/src/components/TraceBox.jsx
+++ frontend/src/components/TraceBox.jsx
@@ -1,8 +1,17 @@
 import styles from "./TraceBox.module.css";
 
-import { Fragment } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 
 export default function TraceBox({code, lines}) {
+  const lineRefs = useRef([]);
+  const [lineDimensions, setLineDimensions]= useState([]);
+
+  useEffect(() => {
+    setLineDimensions(
+      lineRefs.map((ref) => ref.current.getBoundingClientRect())
+    );
+  }, []);
+
   return (
     <div className={styles.container}>
       <p className={styles.largeText}>Trace execution</p>
@@ -10,21 +19,26 @@ export default function TraceBox({code, lines}) {
         <div className={styles.codeBox}>
           {
             code.map(
-              (line, i) => (
-                <Fragment key={`line-${i}`}>
-                  <span>{`${lines[i]}${lines[i] === "" ? "" : "."}`}</span>
-                  <span id={`code-line-${i}`} className={styles.preserveSpace}>{line}</span>
-                </Fragment>
-              )
+              (line, i) => {
+                const ref = useRef(null);
+                lineRefs.push(ref);
+                return (
+                  <Fragment key={`line-${i}`}>
+                    <span>{`${lines[i]}${lines[i] === "" ? "" : "."}`}</span>
+                    <span className={styles.preserveSpace} ref={ref}>{line}</span>
+                  </Fragment>
+                );
+              }
             )
           } 
         </div>
-        <svg width={100} height={500}>
+        <svg>
           {/* <path d="M 0 0 Q 50 50 0 100 T 0 200 T 0 600" stroke="black" fill="transparent" /> */}
           <path d="M 0 0 Q 50 50 0 100 Q 50 150 0 200" stroke="black" fill="transparent" />
           {/* <path d="M 0 200 Q 50 250 0 300" stroke="black" fill="transparent" /> */}
         </svg>
       </div>
+      {lineDimensions}
     </div>
   );
 } 
