b9ed14e9ae76ffac75d99319c906dfe9393e7b66
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-06 19:23:16 +1000

    need to highlight last line

--- frontend/src/components/TraceBox.jsx
+++ frontend/src/components/TraceBox.jsx
@@ -2,7 +2,10 @@ import styles from "./TraceBox.module.css";
 
 import { Fragment } from "react";
 
-// return list of string of the path commands that can be joined with .join()
+/**
+ * @param {*} coords object with the starting line and all remaining ones
+ * @returns list of string of the path commands that can be joined with .join()
+ */
 function genSVGPath(coords) {
   const LINE_HEIGHT = 25;
   const path = [`M 0 ${coords.start * LINE_HEIGHT + (LINE_HEIGHT / 2)}`];
@@ -12,7 +15,6 @@ function genSVGPath(coords) {
     path.push(`q 50 ${height / 2} 0 ${height}`);
     prev = coord;
   });
-  console.log(path.join(" "));
   return path;
 }
 
@@ -26,7 +28,11 @@ export default function TraceBox({code, lines, path}) {
             code.map(
               (line, i) => (
                 <Fragment key={`line-${i}`}>
-                  <span>{`${lines[i]}${lines[i] === "" ? "" : "."}`}</span>
+                  <span
+                    // hightlight colour
+                  >
+                    {`${lines[i]}${lines[i] === "" ? "" : "."}`}
+                  </span>
                   <span className={styles.preserveSpace}>{line}</span>
                 </Fragment>
               )
