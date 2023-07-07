f1a4bde7f9ab251bb749fd7a59b20d939a448671
Sunny PC <z5267282@student.unsw.edu.au>
2023-07-08 03:10:37 +1000

    don't need pixels for svg path

--- frontend/src/components/OutputBox.jsx
+++ frontend/src/components/OutputBox.jsx
@@ -2,19 +2,24 @@ import styles from "./OutputBox.module.css";
 
 import { addPixels } from "../helper";
 
+function genArrowPath(width, height, arrow_length) {
+  const verticalLine = `M 0 ${addPixels(width / 2)} l 0 ${addPixels(height)} m 0 ${addPixels(height * -1)}`;
+  return verticalLine;
+}
+
 export default function OutputBox() {
   const SVG_WIDTH = 30;
+  const SVG_HEIGHT = 100;
+  const ARROW_HEAD_LENGTH = 15;
+
   return (
     <label className={styles.container}>
       Output
       <div className={styles.outputBox}>
         <textarea className={styles.uploadBox} value="out" spellCheck={false} readOnly/>
         <svg>
-          <path d=""></path>
+          <path d={genArrowPath(SVG_WIDTH, SVG_HEIGHT, ARROW_HEAD_LENGTH)} stroke="black" fill="transparent"></path>
         </svg>
-        {/* <svg>
-          <path d="M 0 100 l 0 0" stroke="black" fill="transparent"></path>
-        </svg> */}
       </div>
     </label>
   );
