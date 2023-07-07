eec26ab3c9eee78486011364c09f50edeb656f0e
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-08 02:34:20 +1000

    working on svg

--- frontend/src/components/OutputBox.jsx
+++ frontend/src/components/OutputBox.jsx
@@ -1,10 +1,16 @@
 import styles from "./OutputBox.module.css";
 
-export default function VariableBox() {
+export default function OutputBox() {
   return (
     <label className={styles.container}>
       Output
-      <textarea value="out" spellCheck={false} readOnly/>
+      <div className={styles.outputBox}>
+        <p>fish</p>
+        <textarea className={styles.uploadBox} value="out" spellCheck={false} readOnly/>
+        <svg>
+          <path d="M 0 100 l 0 0" stroke="black" fill="transparent"></path>
+        </svg>
+      </div>
     </label>
   );
 };
