1749fd224cdbb9945e2dc4bf9792f495dc6a834a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-12 14:50:29 +1000

    cannot reset settrace inside handler

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,44 +11,45 @@ def main():
     root         : BodyBlock = parse(program)
     lines        : list[Line] = []
     line_mapping : dict[int, Type[Block]] = {}
-    output       : StringIO = StringIO()
+    output       : list[str] = []
     trace_program(trace_line, lines, output)
 
     for l in lines:
         print(l)
 
 class Line:
-    """dataclass to store line information"""
-    def __init__(self, line_no : int, locals : dict, out : str):
-        self.line_no : int  = line_no
+    """A dataclass to store line information"""
+    def __init__(self, line_no : int, locals : dict, out : list[str]):
+        self.line_no : int = line_no
         self.locals  : dict = locals
-        self.out     : str  = out
+        self.out     : list[str] = out
     
     def __str__(self):
         # return f"{self.line_no} : {self.locals}"
         return f"lno: {self.line_no}, out: '{self.out}'"
 
-def trace_program(handler : Callable, lines : list[Line], output : StringIO):
+def trace_program(handler : Callable, lines : list[Line], output : list[str]):
     def wrapper(frame : FrameType, event : str, arg : Any):
         handler(frame, event, arg, lines, output)
         return wrapper
     
-    sys.stdout = output
     sys.settrace(wrapper)
     program()
     sys.settrace(None)
-    sys.stdout = sys.__stdout__
 
-def trace_line(frame : FrameType, event : str, arg : Any, lines, output : StringIO):
+def trace_line(frame : FrameType, event : str, arg : Any, lines, output : list[str]):
     if event != "line":
         return
 
-    lines.append(Line(frame.f_lineno, frame.f_locals, output.getvalue()))
+    line_output : StringIO = StringIO()
+    sys.stdout = line_output
+    output.insert(0, line_output.getvalue())
+    lines.append(Line(frame.f_lineno, frame.f_locals, output))
+    sys.stdout = sys.__stdout__
 
 def generate_execution_graph(root : BodyBlock, lines : list[Line]):
-    """create the intelligent unique execution path
-    return a condensed line list"""
-
+    """Create the intelligent unique execution path.
+    Return a condensed line list."""
     line_mapping : dict[int, Type[Block]] = {}
     root.map_lines(line_mapping)
     curr : Type[Block] | None = None
