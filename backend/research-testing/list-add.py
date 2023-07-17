2fdedbf23b35f0d7b4d6ce0ba21698a18de1f125
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-17 23:21:31 +1000

    list references persist even for classes

--- backend/research-testing/list-add.py
+++ backend/research-testing/list-add.py
@@ -2,9 +2,18 @@ class C:
     def __init__(self, ls : list[int]):
         self.ls = ls
 
-a = [1, 2]
-c = C(a + [1])
-print(c.ls)
-a.append(42)
-print(c.ls)
-print(repr(a), repr(c.ls))
+def class_test():
+    a = [1, 2]
+    c = C(a + [1])
+    print(c.ls)
+    a.append(42)
+    print(c.ls)
+    print(repr(a), repr(c.ls))
+
+def constructor_test():
+    a = [1, 2]
+    c = C(a)
+    a.append(3)
+    print(c.ls)
+
+constructor_test()
