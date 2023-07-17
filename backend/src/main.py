ffa8e0595c12f1b18c37cb589686ecbba4b2db74
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-17 23:38:15 +1000

    adding adjusted programs

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,15 +9,15 @@ from tree import Block, BodyBlock
 from tree_parser import parse
 
 def main():
-    root         : BodyBlock = parse(program)
-    line_mapping : dict[int, Type[Block]] = {}
-    buffer       : StringIO = StringIO()
+    # root         : BodyBlock = parse(program)
+    # line_mapping : dict[int, Type[Block]] = {}
+    # buffer       : StringIO = StringIO()
 
-    lines        : list[Line] = []
-    output       : list[str] = []
-    trace_program(lines, output)
-    for l in lines:
-        print(l)
+    # lines        : list[Line] = []
+    # output       : list[str] = []
+    # for l in lines:
+    #     print(l)
+    pass
 
 if __name__ == '__main__':
     main()
