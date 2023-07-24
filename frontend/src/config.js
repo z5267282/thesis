27812dac0007ae72a3848a039c2ccf60f81ce4fb
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-25 00:06:04 +1000

    changing counter colours

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -8,6 +8,6 @@ export const TRACE_GRAPH_WIDTH = 50;
 // arrow related
 export const ARROW_WIDTH = 30;
 export const ARROW_HEIGHT = 100;
-export const ARROW_HEAD_LENGTH = 10
+export const ARROW_HEAD_LENGTH = 10;
 
-export const COUNTER_COLOURS = ["green", "blue"]
+export const COUNTER_COLOURS = ["lime", "aqua"];
