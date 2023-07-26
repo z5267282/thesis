b4364950ebaa75e15f4569ee0f6bc73fda18ab8f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-26 16:56:54 +1000

    if branch trace is wrong

--- backend/src/analyse.py
+++ backend/src/analyse.py
@@ -4,12 +4,12 @@ from typing import Type
 from line import Line
 from tree import Block, CodeBlock, IfBlock, ElifBlock, ElseBlock, WhileBlock
 
+print_lines = lambda lines, i: print("{} - [{}]".format(i, ", ".join(str(l) for l in lines)))
+
 def smart_trace(line_mapping : dict[int, Type[Block]], lines : list[Line]):
     """From a list of raw Lines of execution, generate an intelligent
     filtering.
     Decompose lines into regions of CodeBlocks, IfBlocks or WhileBlocks."""
-    print_lines = lambda lines : print(", ".join(str(l) for l in lines))
-    print_lines(lines)
 
     filtered : list[Line] = []
     i : int = 0
@@ -25,8 +25,6 @@ def smart_trace(line_mapping : dict[int, Type[Block]], lines : list[Line]):
             filtered.append(won)
             filtered.extend(smart_trace(line_mapping, rest))
         elif isinstance(block, WhileBlock):
-            print("here!")
-            print_lines(region)
             paths , _ = trace_while(region, block.start)
             for path in paths:
                 filtered.append(path.pop(0))
@@ -44,7 +42,6 @@ def find_region(lines : list[Line], end : int, start : int):
         region.append(lines[i])
         i += 1
 
-    print(f"in find_region: {i}")
     return region, i
 
 def trace_code_block(region: list[Line]):
@@ -63,6 +60,7 @@ def trace_if(lines: list[Line], root : IfBlock):
         line_no : int = line.line_no
         MaybeConditional = IfBlock | ElifBlock | ElseBlock | None 
         branch : MaybeConditional = root.find_branch(line_no)
+        print(f"{line_no} - {type(branch)}")
         if branch is not None:
             won = line
         elif line_no == branch.get_top().start:
@@ -74,21 +72,22 @@ def trace_if(lines: list[Line], root : IfBlock):
 def trace_while(lines : list[Line], start : int):
     all_paths : list[Line] = []
     curr : list[Line] = []
-    for i, line in enumerate(lines):
+    for line in lines:
         # start of new path
         # must make sure we're not on the first line of the whole while
-        if i == start and curr:
+        if line.line_no == start and curr:
             all_paths.append(curr)
-            curr.clear()
+            curr = []
 
         curr.append(line)
-
+    
     # should not filter any lines from a non-taken while
     if not all_paths:
         return []
     
     # last path will be just the while
     all_paths.pop(-1)
+
     paths: list[list[Line]] = []
     counters : list[Fraction] = []
     n : int = len(all_paths)
