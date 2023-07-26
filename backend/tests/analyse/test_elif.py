ca84a54340a57e2ee649f744b269867eaf1cf3d4
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-26 23:39:20 +1000

    elif test works

---
+++ backend/tests/analyse/test_elif.py
@@ -0,0 +1,28 @@
+from typing import Type
+
+from analyse import smart_trace
+from execute import trace_program
+from line import Line
+from tree import Block, BodyBlock
+from tree_parser import parse
+
+def program():
+    i = 15
+    if i % 2 == 0:
+        print("two")
+    elif i % 5 == 0:
+        print("woot a five")
+        print(":)")
+
+def test_if():
+    root : BodyBlock = parse(program)
+    line_mapping : dict[int, Type[Block]] = {}
+    root.map_lines(line_mapping)
+    lines : list[Line] =  trace_program(program)
+    filtered : list[Line] = smart_trace(line_mapping, lines)
+
+    assert len(filtered) == 3
+
+    assert filtered[0].line_no == 10
+    assert filtered[1].line_no == 13
+    assert filtered[2].line_no == 15
