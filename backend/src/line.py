36b7e40a2fa8ffa9bb77d842824e1887b514ed58
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-26 22:49:56 +1000

    reverting back to normal implementation

--- backend/src/line.py
+++ backend/src/line.py
@@ -17,16 +17,5 @@ class Line:
         - prev: {self.vars.prev}
         - curr: {self.vars.curr}"""
     
-    def line_no_equal(self, other):
+    def __eq__(self, other : "Line"):
         return self.line_no == other.line_no
-
-    @classmethod
-    def lines_equal(lhs : list["Line"] , rhs : list["Line"]):
-        if len(lhs) != len(rhs):
-            return False
-    
-        for left, right in zip(lhs, rhs):
-            if not lhs.line_no_equal(rhs):
-                return False
-        
-        return True
