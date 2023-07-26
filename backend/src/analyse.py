ed1a9ad1ce0f9458841ea2efea27da570a3fabea
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-26 13:47:34 +1000

    first serious attempt at parse

--- backend/src/analyse.py
+++ backend/src/analyse.py
@@ -5,19 +5,17 @@ from tree import Block, CodeBlock, IfBlock, ElifBlock, ElseBlock, WhileBlock
 
 def smart_trace(line_mapping : dict[int, Type[Block]], lines : list[Line]):
     """From a list of raw Lines of execution, generate an intelligent
-    filtering"""
+    filtering.
+    Decompose lines into regions of CodeBlocks, IfBlocks or WhileBlocks."""
     filtered : list[Line] = []
-    i = 0
+    i : int = 0
     while i < len(lines):
+        # we assume that i is the start of a region
         line  : Line = lines[i]
         block : Type[Block] = line_mapping[line.line_no]
+        region, offset = find_region(lines, block.end, i)
         if isinstance(block, CodeBlock):
-            filtered.append(line)
-            j = i
-            # find the end of the block
-            while lines[j].line_no < block.end:
-                j += 1
-            filtered.append(lines[j])
+            filtered.extend(trace_code_block(region))
         elif isinstance(block, IfBlock):
             # TODO
             pass
@@ -29,6 +27,28 @@ def smart_trace(line_mapping : dict[int, Type[Block]], lines : list[Line]):
                 j += 1
             lines.extend(trace_while(while_lines))
 
+def find_region(lines : list[Line], end : int, start : int):
+    """Return a region and the next line in lines to go to"""
+    region : list[Line] = []
+    i : int = start
+    while i < len(lines) and lines[i].line_no <= end:
+        region.append(lines[i])
+        i += 1
+    return region, i
+
+def trace_code_block(region: list[Line]):
+    last : Line = region[-1]
+    return [last]
+
+def find_last(lines : list[Line], end : int):
+    last = None
+    i = 0
+    while list[i].line_no <= end:
+        last = list[i]
+        i += 1
+    return last
+
+
 def trace_if(line_mapping : dict[int, Type[Block]], lines: list[Line], root : IfBlock):
     """
         Given all lines related to an if statement, filter out the winning path
@@ -36,7 +56,6 @@ def trace_if(line_mapping : dict[int, Type[Block]], lines: list[Line], root : If
     MaybeConditional = IfBlock | ElifBlock | ElseBlock | None 
     won : Line | None = None
     result : list[Line] = [] 
-    is_conditional : Callable = lambda block : isinstance(block, (IfBlock, ElifBlock, ElseBlock))
 
     i : int = 0
     while i < len(lines):
