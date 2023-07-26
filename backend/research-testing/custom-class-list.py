a8ffbfc0c52966ec7470dbf50958d70d49d1cdf9
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-26 15:15:55 +1000

    probably not a good idea to be hashing the lines

--- backend/research-testing/custom-class-list.py
+++ backend/research-testing/custom-class-list.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 class Line:
     def __init__(self, start, end):
         self.start = start
@@ -17,4 +19,19 @@ def list_test():
 
     print(l1 == l2)
 
-list_test()
+def od_test():
+    a = Line(1, 1)
+    b = Line(2, 2)
+    l1 = (a, b)
+    assert isinstance(l1, tuple)
+    od = OrderedDict()
+    od[l1] = 2
+
+    c = Line(1, 1)
+    d = Line(2, 2)
+    l2 = (c, d)
+
+    print(l2 in od)
+
+# list_test()
+od_test()
