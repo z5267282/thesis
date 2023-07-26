073452ef25ea901934bb6dbf7703492d55283ba4
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-26 23:28:24 +1000

    fixing script

--- backend/src/map.sh
+++ backend/src/map.sh
@@ -2,6 +2,6 @@ i=0
 while [ $i -lt 12 ]
 do
     n=$(sed -n $((i + 1))p line-nums.txt)
-    echo "assert filtered[$i].line_no == $((n + 7))"
+    echo "assert filtered[$i].line_no == $((n + 8))"
     i=$((i + 1))
 done
--- backend/tests/analyse/test_while_if.py
+++ backend/tests/analyse/test_while_if.py
@@ -20,20 +20,6 @@ def program():
     
     print(f"2s: {twos}, 7s: {sevens}")
 
-"""Expected:
-3
-4
-11
-4
-5
-7
-11
-4
-8
-10
-11
-13"""
-
 def test_while_if():
     root : BodyBlock = parse(program)
     line_mapping : dict[int, Type[Block]]
@@ -43,15 +29,15 @@ def test_while_if():
 
     assert len(filtered == 12)
 
-    assert filtered[0].line_no == 10
-    assert filtered[1].line_no == 11
-    assert filtered[2].line_no == 18
-    assert filtered[3].line_no == 11
-    assert filtered[4].line_no == 12
-    assert filtered[5].line_no == 14
-    assert filtered[6].line_no == 18
-    assert filtered[7].line_no == 11
-    assert filtered[8].line_no == 15
-    assert filtered[9].line_no == 17
-    assert filtered[10].line_no == 18
-    assert filtered[11].line_no == 20
+    assert filtered[0].line_no == 11
+    assert filtered[1].line_no == 12
+    assert filtered[2].line_no == 19
+    assert filtered[3].line_no == 12
+    assert filtered[4].line_no == 13
+    assert filtered[5].line_no == 15
+    assert filtered[6].line_no == 19
+    assert filtered[7].line_no == 12
+    assert filtered[8].line_no == 16
+    assert filtered[9].line_no == 18
+    assert filtered[10].line_no == 19
+    assert filtered[11].line_no == 21
