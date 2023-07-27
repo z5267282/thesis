6eb677e0e8413af8e125969b406bbe77a6c2ed1a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-27 22:53:39 +1000

    fixing html for in variable box

--- frontend/src/components/VariableBox.jsx
+++ frontend/src/components/VariableBox.jsx
@@ -2,9 +2,9 @@ import styles from "./VariableBox.module.css";
 
 export default function VariableBox({variables}) {
   return (
-    <label className={styles.container}>
+    <label className={styles.container} htmlfor="variable-box">
       <h2 className={styles.fontSize}>Variables</h2>
-      <textarea id="outputBox" value={variables.join("\n")} spellCheck={false} readOnly/>
+      <textarea id="variable-box" value={variables.join("\n")} spellCheck={false} readOnly/>
     </label>
   );
 };
