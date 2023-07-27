a2dd5297672613671145774a3bc8bed0ee7335ed
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-27 22:55:45 +1000

    making variable box non-resizeable

--- frontend/src/components/VariableBox.jsx
+++ frontend/src/components/VariableBox.jsx
@@ -4,7 +4,7 @@ export default function VariableBox({variables}) {
   return (
     <label className={styles.container} htmlfor="variable-box">
       <h2 className={styles.fontSize}>Variables</h2>
-      <textarea id="variable-box" value={variables.join("\n")} spellCheck={false} readOnly/>
+      <textarea id="variable-box"  className={styles.noResize} value={variables.join("\n")} spellCheck={false} readOnly/>
     </label>
   );
 };
