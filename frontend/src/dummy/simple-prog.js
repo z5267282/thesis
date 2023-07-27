897bfe80581cf49ad27dfcdd1baaff8d6a735215
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-28 00:55:14 +1000

    full dataframe testing

---
+++ frontend/src/dummy/simple-prog.js
@@ -0,0 +1,96 @@
+/**
+    i = 7
+    print("doing some counting")
+    if i % 2 == 0:
+	    print("a multiple of 2")
+    elif i % 7 == 0:
+	    print("a holy number!")
+	    print("seven")
+ */
+
+const data = [
+    {
+        code: [
+            "i = 7",
+            "print(\"doing some counting\")",
+            "if i % 2 == 0:",
+            "    ...",
+            "elif i % 7 == 0:",
+            "    ...",
+        ],
+        lines: [
+            "1", "2", "3", "", "4", ""
+        ],
+        curr: 0,
+        vars: ["i = 7"],
+        out: [],
+        path: null,
+        counter: []
+    },
+    {
+        code: [
+            "i = 7",
+            "print(\"doing some counting\")",
+            "if i % 2 == 0:",
+            "    ...",
+            "elif i % 7 == 0:",
+            "    ...",
+        ],
+        lines: [
+            "1", "2", "3", "", "4", ""
+        ],
+        curr: 1,
+        vars: ["i = 7"],
+        out: ["doing some counting"],
+        path: {
+            start: 0, rest: [1]
+        },
+        counter: []
+    },
+    {
+        code: [
+            "i = 7",
+            "print(\"doing some counting\")",
+            "if i % 2 == 0:",
+            "    ...",
+            "elif i % 7 == 0:",
+            "    ...",
+        ],
+        lines: [
+            "1", "2", "3", "", "4", ""
+        ],
+        curr: 4,
+        vars: ["i = 7"],
+        out: ["doing some counting"],
+        path: {
+            start: 0, rest: [1, 4]
+        },
+        counter: []
+    },
+    {
+        code: [
+            "i = 7",
+            "print(\"doing some counting\")",
+            "if i % 2 == 0:",
+            "    ...",
+            "elif i % 7 == 0:",
+            "    print(\"a holy number!\")",
+            "    print(\"seven\")"
+        ],
+        lines: [
+            "1", "2", "3", "", "4", "5", "6"
+        ],
+        curr: 5,
+        vars: ["i = 7"],
+        out: [
+            "seven",
+            "a holy number!",
+            "doing some counting"
+        ],
+        path: {
+            start: 0, rest: [1, 4, 5]
+        },
+        counter: []
+    },
+];
+export default data;
