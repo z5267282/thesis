89bc42d9cd652be6a81381b0fc990467aea7f16d
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-29 00:05:47 +1000

    must have different basename for pytest

---
+++ backend/tests/line_mapping/test_if.py
@@ -0,0 +1,18 @@
+from tree import CodeBlock, IfBlock
+
+def test_if():
+    body = CodeBlock(2)
+    body.end = 4
+
+    if_ = IfBlock(1, 0)
+    if_.end = 4
+    if_.add_same_level_block(body)
+
+    line_mapping = if_.map_lines()
+
+    assert line_mapping == {
+        1 : if_,
+        2 : body,
+        3 : body,
+        4 : body
+    }
