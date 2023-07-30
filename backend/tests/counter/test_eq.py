b5df13d8740aa74f70634e39d14cc45bf1261abd
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-30 12:14:08 +1000

    counter eq testing done

--- backend/tests/counter/test_eq.py
+++ backend/tests/counter/test_eq.py
@@ -9,3 +9,32 @@ def test_eq_same_while():
     c2 = Counter(4, 15, w)
 
     assert c1 == c2
+
+def test_eq_different_while():
+    w1 = WhileBlock(1, 0)
+    w1.end = 10
+    c1 = Counter(4, 15, w1)
+
+    w2 = WhileBlock(1, 0)
+    w2.end = 10
+    c2 = Counter(4, 15, w2)
+
+    assert c1 == c2
+
+def test_ne_same_while_same_total():
+    w = WhileBlock(1, 0)
+    w.end = 10
+
+    c1 = Counter(5, 15, w)
+    c2 = Counter(4, 15, w)
+
+    assert c1 != c2
+
+def test_ne_same_while_same_iter():
+    w = WhileBlock(1, 0)
+    w.end = 10
+
+    c1 = Counter(4, 60, w)
+    c2 = Counter(4, 15, w)
+
+    assert c1 != c2
