62bc3c79f3d03db17ed459be30f4e404baf0015b
Sunny Chen <z5267282@student.unsw.edu.au>
2023-07-30 14:44:29 +1000

    long string works with one counter

--- backend/tests/line/test_string_methods.py
+++ backend/tests/line/test_string_methods.py
@@ -23,3 +23,20 @@ def test_long_str_no_counters():
         - prev: {i : 1}
         - curr: {i : 2}
     counters: []"""
+
+def test_long_str_counters():
+    l = Line(20, {"i": "1"})
+    l.vars.curr = {"i": "2"}
+    l.output = ["hello", "champion"]
+    w = WhileBlock(1, 0)
+    w.end = 10
+    l.add_counter(1, 5, w)
+
+    assert l.long_str() == """line 20:
+    output: [hello, champion]
+    vars  :
+        - prev: {i : 1}
+        - curr: {i : 2}
+    counters: [
+        (1/5) : WhileBlock(start=1, end=10)
+    ]"""
