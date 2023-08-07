384e000b79f21589fd1137daca3e6f409d98ede1
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-07 23:30:31 +1000

    decomposition is broken on the topmost level

--- backend/tests/execute/test_loop_paths.py
+++ backend/tests/execute/test_loop_paths.py
@@ -19,3 +19,19 @@ def test_no_gaps_2d_while():
     line_mapping = root.map_lines()
     lines = trace_program(program)
     filtered = smart_trace(line_mapping, lines)
+
+    for i in range(1, 6):
+        print("{}: {}".format(i, ", ".join(str(j) for j in lines[i].loop_path)))
+
+# code region
+    assert filtered[0].loop_path == []
+
+    # while region
+    assert filtered[1].loop_path == []
+    assert filtered[2].loop_path == [2]
+    assert filtered[3].loop_path == [2, 3]
+    assert filtered[4].loop_path == [2, 3, 4]
+    assert filtered[5].loop_path == [2, 3, 4, 6]
+
+    # code block
+    assert filtered[6].loop_path == []
