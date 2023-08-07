dc3b91b9340f8e535de09b32f64ae57c7ca19e4e
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-08 01:21:52 +1000

    need to reverse path

--- backend/research-testing/dequeue.py
+++ backend/research-testing/dequeue.py
@@ -1,5 +1,15 @@
 from collections import deque
 
-d = deque()
-d.append(1)
-print(len(d))
+def append():
+    d = deque()
+    d.append(1)
+    print(len(d))
+
+def extendleft():
+    d = deque()
+    ls = [1, 2, 3]
+    ls.reverse()
+    d.extendleft(ls)
+    print(", ".join(str(i) for i in d))
+
+extendleft()
