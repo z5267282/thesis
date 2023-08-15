129854c433d77eafd26146b3078a3f19beff77b9
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-16 02:07:40 +1000

    top level test works

---
+++ backend/tests/execute/graph/test_graph_ifs.py
@@ -0,0 +1,35 @@
+from analyse import smart_trace
+from execute import trace_program
+from graph import generate_graphs
+from line import Line
+from tree_parser import parse
+
+def program():
+    i = 17
+    if i % 2 == 0:
+        print("found a two")
+    elif i % 17 == 0:
+        print("a fancy prime")
+        print("fancy!")
+    print("end of day!")
+
+def test_graph_simple():
+    root = parse(program)
+    lines = trace_program(program)
+    line_mapping = root.map_lines()
+    filtered = smart_trace(line_mapping, lines)
+    graphs = generate_graphs(filtered, line_mapping)
+
+    assert filtered == [
+        Line(8, {}),
+        Line(11, {}),
+        Line(13, {}),
+        Line(14, {})
+    ]
+
+    assert graphs == [
+        [8],
+        [8, 11],
+        [8, 11, 13],
+        [8, 11, 13, 14]
+    ]
