5966c9cf9a87f9d43ea12b137b9657b2c96c9671
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-16 00:43:17 +1000

    first draft of the graph generation

--- backend/src/graph.py
+++ backend/src/graph.py
@@ -1,35 +1,57 @@
-from collections import namedtuple
 from typing import Type
 
+from line import Line
 from stack import Stack
-from tree import Block, BodyBlock, WhileBlock
+from tree import Block, WhileBlock
+
+def generate_graphs(
+    filtered : list[Line], line_mapping : dict[int, Type[Block]]
+):
+    top_level : list[int] = []
+    whiles    : Stack[While] = Stack[While]()
+    return [
+        generate_ith_graph(
+            f.line_no, line_mapping[f.line_no], top_level, whiles
+        ) for f in filtered
+    ]
 
 class While:
     def __init__(self, node : WhileBlock):
         self.node  : WhileBlock = node
         self.lines : list[int] = []
+
+def generate_ith_graph(
+    line_no : int, node : Type[Block],
+    top_level : list[int], whiles : Stack[While]
+):
+    """Generate the execution graph up to the ith line.
+    Return a list of raw line numbers to show in the graph."""
+    result = lambda: generate_from_state(top_level, whiles)
+
+    while whiles:
+        top : While = whiles.peek()
+        curr : WhileBlock = top.node
+        if line_no == curr.start:
+            top.lines.clear()
+            return result()
+
+        if line_no <= curr.end:
+            break
     
+    # the top of the stack now stores the correct item
+    if isinstance(node, WhileBlock):
+        whiles.push(While(node))
+    elif whiles:
+        top : While = whiles.peek()
+        top.lines.append(line_no)
+    else:
+        top_level.append(line_no)
+    return result()
+
 def generate_from_state(top_level : list[int], whiles : Stack[While]):
     result : list[int] = list.copy()
     # stack supports linear iteration from bottom to top
     for w in whiles:
         result.append(w.node.start)
-        result.extend(w)
-
-def generate_ith_graph(
-    line_no : int, line_mapping : dict[int, Type[Block]],
-    top_level : list[int], whiles : Stack[While]
-):
-    """Generate the execution graph up to the ith line.
-    Return a list of raw line numbers to show in the graph."""
-    if whiles:
-        while whiles:
-            top : While = whiles.peek()
-            node : WhileBlock = top.node
-            if line_no == node.start:
-                pass
-            # if line_no == node.start or line_no > node.end:
-            #     break
-            # whiles.pop()
-
-    # if 
+        result.extend(w.lines)
+    return result
