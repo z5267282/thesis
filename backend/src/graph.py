81c51cc3f78a3cbab1ee29288f2ca4e136a9ea0e
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-15 23:35:56 +1000

    stack popping done

--- backend/src/graph.py
+++ backend/src/graph.py
@@ -2,12 +2,23 @@ from collections import namedtuple
 
 from line import Line
 from stack import Stack
-from tree import WhileBlock
+from tree import BodyBlock, WhileBlock
 
 class While:
     def __init__(self, node : WhileBlock):
         self.node  : WhileBlock = node
         self.lines : list[int] = []
-
-def generate_line_execution_graph(index : int, filtered : list[Line], top_level : list[int]):
     
+def generate_ith_graph(
+    line_no : int, root : BodyBlock,
+    top_level : list[int], whiles : Stack[While]
+):
+    if whiles:
+        while whiles:
+            top : While = whiles.peek()
+            node : WhileBlock = top.node
+            if line_no == node.start or line_no > node.end:
+                break
+            whiles.pop()
+
+    if 
