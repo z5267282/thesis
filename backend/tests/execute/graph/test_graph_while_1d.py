40f9bcbe2ab81238c80801e9b4714d4e85c2c170
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-16 12:12:19 +1000

    woot

---
+++ backend/tests/execute/graph/test_graph_while_1d.py
@@ -0,0 +1,56 @@
+from analyse import smart_trace
+from execute import trace_program
+from graph import generate_graphs
+from line import Line
+from tree_parser import parse
+
+def program():
+    i = 1
+    twos, sevens = 0, 0
+    while i < 8:
+        if i % 2 == 0:
+            print("two")
+            twos += 1
+        elif i % 7 == 0:
+            print("seven")
+            sevens += 1
+        i += 1
+
+    print(f"2s: {twos}, 7s: {sevens}")
+
+def test_graph_simple():
+    root = parse(program)
+    lines = trace_program(program)
+    line_mapping = root.map_lines()
+    filtered = smart_trace(line_mapping, lines)
+    graphs = generate_graphs(filtered, line_mapping)
+
+    assert filtered == [
+        Line(9, {}),
+        Line(10, {}),
+        Line(17, {}),
+        Line(10, {}),
+        Line(11, {}),
+        Line(13, {}),
+        Line(17, {}),
+        Line(10, {}),
+        Line(14, {}),
+        Line(16, {}),
+        Line(17, {}),
+        Line(19, {})
+    ]
+
+    assert graphs == [
+        [9],
+        [9, 10],
+        [9, 10, 17],
+        [9, 10],
+        [9, 10, 11],
+        [9, 10, 11, 13],
+        [9, 10, 11, 13, 17],
+        [9, 10],
+        [9, 10, 14],
+        [9, 10, 14, 16],
+        [9, 10, 14, 16, 17],
+        [9, 19]
+    ]
