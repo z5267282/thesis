4dd8cc85bf3da7d89f0cc824b2deec5b427e9039
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-16 11:36:35 +1000

    using a generic state

--- backend/src/state.py
+++ backend/src/state.py
@@ -1,10 +1,12 @@
-from typing import Any
+from typing import TypeVar
+
+T = TypeVar("T")
 
 class State:
     """dataclass to track previous and current state"""
-    def __init__(self, prev : Any, curr : Any=None):
-        self.prev : Any = prev
-        self.curr : Any = curr
+    def __init__(self, prev : T, curr : T=None):
+        self.prev : T = prev
+        self.curr : T = curr
 
     def __eq__(self, other : "State"):
         return self.prev == other.prev and self.curr == other.curr
