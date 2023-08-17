9e08285aac2a7c0d954ebe94cfd6505a3ac547ab
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 15:20:04 +1000

    uniq testing done

--- backend/tests/helper/test_uniq.py
+++ backend/tests/helper/test_uniq.py
@@ -27,3 +27,57 @@ def test_leading_falses():
     exp[12] = True
 
     assert uniq(original) == exp
+
+def test_singular_falses():
+    original = OrderedDict()
+    original[1] = False
+    original[2] = True
+    original[3] = False
+    original[4] = True
+    original[5] = False
+
+    exp = OrderedDict()
+    exp[1] = False
+    exp[2] = True
+    exp[3] = False
+    exp[4] = True
+    exp[5] = False
+
+    assert uniq(original) == exp
+
+def test_middle_falses():
+    original = OrderedDict()
+    original[1] = True 
+    original[2] = True
+    original[3] = False
+    original[4] = False
+    original[5] = True
+
+    exp = OrderedDict()
+    exp[1] = True
+    exp[2] = True
+    exp[3] = False
+    exp[5] = True
+
+    assert uniq(original) == exp
+
+def test_integration():
+    original = OrderedDict()
+    original[0] = True
+    original[1] = True
+    original[2] = False
+    original[3] = False
+    original[4] = False
+    original[5] = True
+    original[6] = False
+    original[7] = True
+
+    exp = OrderedDict()
+    exp[0] = True
+    exp[1] = True
+    exp[2] = False
+    exp[5] = True
+    exp[6] = False
+    exp[7] = True
+
+    assert uniq(original) == exp
