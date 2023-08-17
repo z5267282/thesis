5dd7ea72e526ef0bb6f8f3ad76200a5db87a9d03
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 14:55:39 +1000

    moving uniq filtering to helper

--- backend/src/helper.py
+++ backend/src/helper.py
@@ -1,4 +1,4 @@
-import re
+from collections import OrderedDict
 
 def find_first_nospace(string : str):
     """a crude way of finding the first non-space character"""
@@ -15,3 +15,14 @@ def num_leading_whitespace(string : str):
 
 def get_stripped_line(string : str):
     return string[find_first_nospace(string):]
+
+def uniq(show : OrderedDict[int, bool]):
+    """Remove all consecutive False keys with the first occuring one"""
+    filtered : OrderedDict[int, bool] = OrderedDict()
+    prev_false = False
+    for line, shown in show.items():
+        if shown or not prev_false:
+            filtered[line] = shown
+            prev_false = False
+    
+    return filtered
