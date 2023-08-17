2675efccc21cfcd467d7f69d83526e1573b54004
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 14:13:53 +1000

    hiding works on face value

--- backend/tests/tree_show_lines/test_while_if.py
+++ backend/tests/tree_show_lines/test_while_if.py
@@ -25,9 +25,33 @@ def program():
 def test_graph_simple():
     root = parse(program)
 
-    seen = OrderedDict()
+    show = OrderedDict()
     for i in range(root.start, root.end + 1):
-        seen[i] = False
+        show[i] = False
     
-    root.show_lines() 
+    graph = [7, 8, 9, 11, 16, 17, 19]
+    root.show_lines(graph, show) 
 
+    exp = OrderedDict()
+    exp[6] = True
+    exp[7] = True
+    exp[8] = True
+    exp[9] = True
+    exp[10] = True
+    exp[11] = True
+    exp[12] = True
+    exp[13] = False
+    exp[14] = False
+    # whitespace should be considered part of the line 12 elif
+    exp[15] = False
+    exp[16] = True
+    exp[17] = True
+    exp[18] = True
+    exp[19] = True
+    # whitespace should be part of the line 17 while
+    exp[20] = True
+    exp[21] = True
+    exp[22] = True
+    exp[23] = True
+
+    assert show == exp
