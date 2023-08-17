9da2fb2b08af81d1673636641bc97e8b70679f88
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 15:06:18 +1000

    uniq test is broken

--- backend/tests/helper/test_uniq.py
+++ backend/tests/helper/test_uniq.py
@@ -5,3 +5,12 @@ from helper import uniq
 def test_no_falses():
     original = OrderedDict((i, True) for i in range(10))
     exp = OrderedDict((i, True) for i in range(10))
+
+    uniq(original) == exp
+
+def test_all_falses():
+    original = OrderedDict((i, False) for i in range(10))
+    exp = OrderedDict()
+    exp[0] = False
+    
+    assert uniq(original) == exp
