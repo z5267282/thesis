950f82314ec5140ca9b52243cb86a6a3184c420f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 13:18:30 +1000

    making function syntactically ok

--- backend/src/collapse.py
+++ backend/src/collapse.py
@@ -8,3 +8,4 @@ def filter(graph : list[int], root : BodyBlock):
     """Return a list of n booleans where n is the number of lines in a
     program where the ith boolean is whether the ith line is to be shown"""
     # 1-indexed to make life easy
+    pass
