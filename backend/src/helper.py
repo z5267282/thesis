8dc979939cf1e1c2fd0bea33a825070875ae5656
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 14:57:37 +1000

    changing param names to be more generic

--- backend/src/helper.py
+++ backend/src/helper.py
@@ -16,13 +16,13 @@ def num_leading_whitespace(string : str):
 def get_stripped_line(string : str):
     return string[find_first_nospace(string):]
 
-def uniq(show : OrderedDict[int, bool]):
+def uniq(mapping : OrderedDict[int, bool]):
     """Remove all consecutive False keys with the first occuring one"""
     filtered : OrderedDict[int, bool] = OrderedDict()
     prev_false = False
-    for line, shown in show.items():
-        if shown or not prev_false:
-            filtered[line] = shown
+    for index, value in mapping.items():
+        if value or not prev_false:
+            filtered[index] = value
             prev_false = False
     
     return filtered
