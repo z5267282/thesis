f202f29dfc18efdc2de6750b1aff7a7a4049b9d1
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 22:10:37 +1000

    complex path works

--- backend/tests/execute/collapse/test_nested_path.py
+++ backend/tests/execute/collapse/test_nested_path.py
@@ -13,37 +13,61 @@ def program():
         elif i % 5 == 0:
             print("five")
             fives += 1
-
+    
         j = 0
         while j < i:
             print("X", end="")
             j += 1
-
+    
         i += 1
-
+    
     print(f"2s: {twos}, 5s: {fives}")
 
 def test_graph_simple():
     root = parse(program)
     program_code = get_code_info(program)
 
-    graph = [8, 9, 10, 12, 17, 22]
+    graph = [8, 9, 13, 15, 17, 18, 20]
     code, lines, rest = collapse(graph, program_code, root)
 
     assert code == [
         "i = 1",
         "twos, fives = 0, 0",
         "while i < 6:",
-        f"    {ELLIPSE}",
+        "    if i % 2 == 0:",
+        f"        {ELLIPSE}",
+        "    elif i % 5 == 0:",
+        "        print(\"five\")",
+        "        fives += 1",
+        "",
+        "    j = 0",
+        "    while j < i:",
+        "        print(\"X\", end=\"\")",
+        "        j += 1",
+        "",
+        "    i += 1",
+        "",
         "print(f\"2s: {twos}, 5s: {fives}\")"
     ]
 
     assert lines == [
-        7, 
+        7,
         8,
         9,
+        10,
         None,
-        24
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        24,
     ]
 
-    assert rest == [1]
+    assert rest == [1, 2, 5, 7, 9, 10, 12]
