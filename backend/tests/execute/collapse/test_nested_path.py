fb334399bd2f3a0ad48dd54973cee277fba44001
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-17 19:08:23 +1000

    should do an if elif chain test first

---
+++ backend/tests/execute/collapse/test_nested_path.py
@@ -0,0 +1,49 @@
+from cfg import ELLIPSE
+from collapse import collapse
+from helper import get_code_info
+from tree_parser import parse
+
+def program():
+    i = 1
+    twos, fives = 0, 0
+    while i < 6:
+        if i % 2 == 0:
+            print("two")
+            twos += 1
+        elif i % 5 == 0:
+            print("five")
+            fives += 1
+
+        j = 0
+        while j < i:
+            print("X", end="")
+            j += 1
+
+        i += 1
+
+    print(f"2s: {twos}, 5s: {fives}")
+
+def test_graph_simple():
+    root = parse(program)
+    program_code = get_code_info(program)
+
+    graph = [8, 9, 10, 12, 17, 22]
+    code, lines, rest = collapse(graph, program_code, root)
+
+    assert code == [
+        "i = 1",
+        "twos, fives = 0, 0",
+        "while i < 6:",
+        f"    {ELLIPSE}",
+        "print(f\"2s: {twos}, 5s: {fives}\")"
+    ]
+
+    assert lines == [
+        7, 
+        8,
+        9,
+        None,
+        24
+    ]
+
+    assert rest == [1]
