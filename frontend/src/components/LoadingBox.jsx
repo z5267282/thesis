ced8f99ea54b16f55211546aa0af44d38c0a8595
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-18 23:13:07 +1000

    looking good

--- frontend/src/components/LoadingBox.jsx
+++ frontend/src/components/LoadingBox.jsx
@@ -1,7 +1,9 @@
 import styles from "./LoadingBox.module.css";
 
 export default function LoadingBox() {
-  return <div className={styles.loadingBox}>
-    [ enter code for execution ]
+  return <div className={styles.container}>
+    <div className={styles.loadingBox}>
+      [ enter code for execution ]
+    </div>
   </div>
 }
