cc3aa5ad501406a9e620f2a4c1b3fbc64f85f825
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-19 21:40:37 +1000

    adding demo files for debugging

---
+++ frontend/src/demo/1-top-level.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        code: [
+            "i = 42",
+            "j = 69",
+            "print(f\"{i} + {j} = {i + j}\")",
+        ],
+        lines: [
+            "1",
+            "2",
+            "3",
+        ],
+        curr: null,
+        vars: [],
+        out: [],
+        path: {
+            start: 0,
+            rest: []
+        },
+        counters: []
+    }
+];
+
+export default data;
