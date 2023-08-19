a45950a5b080db42663b7b29b1292f6301912050
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-19 23:20:34 +1000

    hack; buttons stay at the top for now

--- frontend/src/components/TraceBox.jsx
+++ frontend/src/components/TraceBox.jsx
@@ -12,16 +12,6 @@ export default function TraceBox({
       <h1 className={styles.largeText}>
         Trace execution
       </h1>
-      {
-        (code.length === 0) ?
-          <LoadingBox />
-        :
-          <TracedLinesBox
-            code={code} lines={lines} curr={curr} path={path}
-            lineHeight={lineHeight} fontScaling={fontScaling}
-            graphWidth={graphWidth} counters={counters} counterColours={counterColours}
-          />
-      }
       <div className={styles.transitionContainer}>
         <button
           onClick={() => changeIndex(-1)} disabled={disablePrev}
@@ -36,6 +26,16 @@ export default function TraceBox({
           next
         </button>
       </div>
+      {
+        (code.length === 0) ?
+          <LoadingBox />
+        :
+          <TracedLinesBox
+            code={code} lines={lines} curr={curr} path={path}
+            lineHeight={lineHeight} fontScaling={fontScaling}
+            graphWidth={graphWidth} counters={counters} counterColours={counterColours}
+          />
+      }
     </div>
   );
 } 
