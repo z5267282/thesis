e960fda1d5844e18d59e75e47b102617abc2c56a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-20 21:01:42 +1000

    very dodgy way of doing expressions for now

--- backend/src/evaluate.py
+++ backend/src/evaluate.py
@@ -1,6 +1,8 @@
-def evaluate(expression : str, variables : dict[str, str]):
+from typing import Any
+
+def evaluate(expression : str, variables : dict[str, Any]):
     """For now, we only support 1 variable subsitution""" 
+    result = expression
     for name, value in variables.items():
-        if name in expression:
-            return expression.replace(name, str(value))
-    return expression
+        result = result.replace(name, str(value))
+    return result
