8ed9950cd83e88578561b3bd192af16faa63450a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-20 13:56:07 +1000

    starting on generate refactoring

---
+++ backend/tests/execute/generate/test_loop_conditional.py
@@ -0,0 +1,19 @@
+def program():
+    i = 1
+    twos, fives = 0, 0
+    while i < 6:
+        if i % 2 == 0:
+            print("two")
+            twos += 1
+        elif i % 5 == 0:
+            print("five")
+            fives += 1
+    
+        j = 0
+        while j < i:
+            print("X", end="")
+            j += 1
+    
+        i += 1
+    
+    print(f"2s: {twos}, 5s: {fives}")
