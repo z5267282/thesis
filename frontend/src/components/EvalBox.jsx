14d5cd07c3e65db42c40cd82276f819f0475c6e0
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-20 20:44:46 +1000

    fixing formatting

--- frontend/src/components/EvalBox.jsx
+++ frontend/src/components/EvalBox.jsx
@@ -2,14 +2,19 @@ import styles from "./EvalBox.module.css";
 
 export default function EvalBox({evallines}) {
   return (
-    <div className={styles.evalBox}>
-      {
-        evallines.map((line, i) => (
-          <span key={`evalline-${i}`}>
-            {line}
-          </span>
-        ))
-      }
-    </div>
+    <>
+      <h2 className={styles.fontSize}>
+        Conditional Evaluation
+      </h2>
+      <div className={styles.evalBox}>
+        {
+          evallines.map((line, i) => (
+            <span key={`evalline-${i}`}>
+              {line}
+            </span>
+          ))
+        }
+      </div>
+    </>
   )
 }
