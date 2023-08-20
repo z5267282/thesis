3c231facf1478e893ff856abd0fbf1f4bbf9741a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-20 14:19:11 +1000

    everything looks like it should work

--- backend/tests/execute/collapse/test_loop_conditional.py
+++ backend/tests/execute/collapse/test_loop_conditional.py
@@ -18,4 +18,41 @@ def program():
     
     print(f"2s: {twos}, 5s: {fives}")
 
+from collections import OrderedDict
+from typing import Type
+
+from analyse import smart_trace
+from collapse import collapse
+from counter import Counter
+from dataframe import DataFrame
+from graph import generate_graphs
+from helper import get_code_info, get_stripped_line
+from line import Line
+from evaluate import evaluate
+from execute import trace_program
+from tree import Block, BodyBlock
+from tree_parser import parse
+
 from generate import generate_dataframes
+
+def test_on_while_line():
+    root : BodyBlock = parse(program)
+    line_mapping : dict[int, Type[Block]] = root.map_lines()
+    all_lines : list[Line] = trace_program(program)
+    filtered : list[Line] = smart_trace(line_mapping, all_lines)
+    line_graphs : list[list[Line]] = generate_graphs(filtered, line_mapping)
+    program_code : OrderedDict[int, str] = get_code_info(program)
+
+    assert line_graphs[:2] == [
+        [Line(3, {})],
+        [Line(3, {}), Line(4, {})]
+    ]
+
+    _, graph = line_graphs[:2]
+    code, lines, path = collapse(graph, program_code, root)
+    _, curr = graph
+    assert len(curr.counters) == 1
+    counter, = curr.counters
+    assert counter.start is None
+    assert counter.end is None
+    assert not counter.has_valid_range()
