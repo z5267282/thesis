244eab5ef3d0ff3a4a296c50c0e07174db0d19c4
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-20 14:30:06 +1000

    now ready to proceed with integration tests

---
+++ backend/tests/execute/generate/test_loop_conditional.py
@@ -0,0 +1,24 @@
+def program():
+    i = 1
+    twos, fives = 0, 0
+    while i < 6:
+        if i % 2 == 0:
+            print("two")
+            twos += 1
+        elif i % 5 == 0:
+            print("five")
+            fives += 1
+    
+        j = 0
+        while j < i:
+            print("X", end="")
+            j += 1
+    
+        i += 1
+    
+    print(f"2s: {twos}, 5s: {fives}")
+
+from generate import generate_dataframes
+
+def test_loop_conditional():
+    dataframes = generate_dataframes(program)
