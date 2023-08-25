7c929cb03f1728e73b3afef7b8d068bff0353a77
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-25 15:08:44 +1000

    adding else branch testing

---
+++ backend/tests/execute/test_if_else.py
@@ -0,0 +1,13 @@
+def program():
+    i = 0
+    if i == 1:
+        print("one")
+    else:
+        print("something else")
+
+from execute import trace_program
+from line import Line
+
+def test_if_else():
+    lines = trace_program(program)
+    assert lines == [ Line(2, {}), Line(3, {}), Line(5, {}) ]
