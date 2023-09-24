ef9f042030572e92024e6ff1660ba951c12c7e8f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-09-24 22:45:45 +1000

    printing out just the one key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,7 +32,7 @@ export default function App() {
   return (
     <div className={styles.App}>
       <div>The server is: "{SERVER}"</div>
-      <div>environment : "{process.env}"</div>
+      <div>environment : "{process.env.THESIS_HOST}"</div>
       <CodeBox
         code={dataFrame.code} lines={dataFrame.lines} path={dataFrame.path}
         counters={dataFrame.counters} curr={dataFrame.curr} counterColours={COUNTER_COLOURS}
