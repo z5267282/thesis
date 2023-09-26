195a52abdc225ef41c20013086bf5c55ca69cb99
Sunny Chen <z5267282@student.unsw.edu.au>
2023-09-26 21:23:47 +1000

    fixing loading box message

--- frontend/src/components/LoadingBox.jsx
+++ frontend/src/components/LoadingBox.jsx
@@ -3,7 +3,7 @@ import styles from "./LoadingBox.module.css";
 export default function LoadingBox() {
   return <div className={styles.container}>
     <div className={styles.loadingBox}>
-      [ enter code for execution ]
+      [ upload your code ]
     </div>
   </div>
 }
