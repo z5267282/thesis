472f40efbdd6c4279ba9b50519cfe6fb87d5ceca
Sunny Chen <z5267282@student.unsw.edu.au>
2023-09-27 16:19:57 +1000

    syntax highlightning

--- frontend/src/components/UploadBox.jsx
+++ frontend/src/components/UploadBox.jsx
@@ -1,7 +1,6 @@
 import Editor from "react-simple-code-editor";
 import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-python';
 import 'prismjs/themes/prism.css';
 
 import { SERVER } from "../config";
@@ -44,7 +43,7 @@ export default function UploadBox({
           id="uploadBox"
           value={traceCode}
           onValueChange={newTraceCode => setTraceCode(newTraceCode)}
-          highlight={code => highlight(code, languages.js)}
+          highlight={code => highlight(code, languages.py)}
           className={styles.editor}
         />
       </div>
