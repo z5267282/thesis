5db285060d613321c0c122af5de84d4fccb4d45b
Sunny Chen <z5267282@student.unsw.edu.au>
2023-09-29 16:44:17 +1000

    fixing q1

--- backend/test-questions/q1.py
+++ backend/test-questions/q1.py
@@ -1,8 +1,9 @@
 i = 1
 while i < 12345:
-    if i % 7841 == 0:
+    if i % 7841 == 0 and i % 23 == 0:
         i += 3
     elif i > 3000:
         i += 2
     else:
         i += 43
+print("that's all folks")
