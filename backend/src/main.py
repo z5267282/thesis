4951c7e3a7ce8014a3dd81ddb9292b66d8ba6d2f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-15 23:40:16 +0800

    trying to add json logic in main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,20 @@
+from json import dumps
 import importlib
 
+from dataframe import DataFrame
 from generate import generate_dataframes
 import program
+import sys
 
 def main():
     """Execute a program and generate a list of DataFrames representing
     execution states."""
     importlib.reload(program)
     return generate_dataframes(program.program)
+
+if __name__ == "__main__":
+    frames : list[DataFrame] = main()
+    json   : str = [ frame.to_dict() for frame in frames ]
+    sys.stdout.write(
+        dumps(json)
+    )
