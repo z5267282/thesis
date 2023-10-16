7641e590d767b218b15d4c56f1d82b6e12b5bd28
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-17 00:21:16 +0800

    fixing timeout to use signal

--- frontend/src/upload/timeout.sh
+++ frontend/src/upload/timeout.sh
@@ -1,12 +1,25 @@
 #!/bin/dash
 
-program="$1"
-
 TIMEOUT_SECS=1
+
+program="$1"
 
 cd /tmp
 python3 raw.py > /dev/null 2>&1 &
+tee raw.py << EOF > /dev/null
+from signal import SIGTERM, signal
+import sys
+signal(
+    SIGTERM, timeout=lambda signum, frame: sys.exit(1)
+)
+$program
+EOF
+
 pid=$!
 sleep $TIMEOUT_SECS
-ps -p $pid > /dev/null && exit 1
+if ps -p $pid > /dev/null
+then
+    kill -s TERM $pid
+    exit 1
+fi
 exit 0
