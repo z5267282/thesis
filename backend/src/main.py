41f36ce006baa8db28a474da5b206180f5751a99
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-16 19:17:08 +0800

    adding documentation comment

--- backend/src/main.py
+++ backend/src/main.py
@@ -20,6 +20,7 @@ def main():
 if __name__ == "__main__":
     frames : list[DataFrame] = main()
     json   : str = [ frame.to_dict() for frame in frames ]
+    # prevent print() behaviour muddling the JSON string
     sys.stdout.write(
         dumps(json)
     )
