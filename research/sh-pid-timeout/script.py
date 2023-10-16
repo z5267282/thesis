4af90cfeb53069b31685070b6d15a39923f1988a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-16 18:12:56 +0800

    modifying script to use exit term

--- research/sh-pid-timeout/script.py
+++ research/sh-pid-timeout/script.py
@@ -1,3 +1,12 @@
+from signal import SIGTERM, signal
+import sys
+
+def timeout(signum, frame):
+    print("timed out!", file=sys.stderr)
+    sys.exit(2)
+
+signal(SIGTERM, timeout)
+
 i = 0
 while True:
     i += 1
--- research/sh-pid-timeout/timeout.sh
+++ research/sh-pid-timeout/timeout.sh
@@ -1,7 +1,5 @@
 python3 script.py &
 pid=$!
 sleep 1
-# term does not kill
-# kill -s TERM $pid
-kill -s KILL $pid
+kill -s TERM $pid
 
