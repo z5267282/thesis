65072c15cfdb76e18a2b3b7020df82d175e38e09
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-16 18:50:53 +0800

    first use of upload

---
+++ frontend/src/upload.sh
@@ -0,0 +1,33 @@
+#!/bin/dash
+
+# usage : dash upload.sh <program>
+
+# feed in a user program and trace
+# use dash in case there are POSIX issues with bash / zsh has
+
+ERR_CLIENT=1
+ERR_TIMEOUT=2
+
+TIMEOUT_SECS=1
+
+program="$1"
+cwd="$(pwd)"
+
+# run program
+cd /tmp
+printf "%s" "$program" > raw.py
+output=$(mktemp)
+trap 'rm -rf $output' EXIT
+# we want to preserve stderr as well
+python3 raw.py > $output 2>&1 &
+status=$?
+pid=$!
+sleep $TIMEOUT_SECS
+if [ $status -ne 0 ]
+then
+    cat $output
+    exit $ERR_CLIENT
+fi
+ps -p $pid && exit $TIMEOUT_SECS
+
+# run trace
