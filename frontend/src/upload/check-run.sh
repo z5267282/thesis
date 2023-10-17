8f59898b8209ca7586f4c891d68c2f7813871945
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-17 18:18:14 +0800

    will shift to using javascript to write files

--- frontend/src/upload/check-run.sh
+++ frontend/src/upload/check-run.sh
@@ -1,6 +1,10 @@
 #!/bin/dash
 
+# see if the program can be run
+# preserve stdout for original caller
+
 # we pass in the program as a string to avoid relying on temporary files
 program="$1"
 
 cd /tmp
+printf '%s' "$program" > 
