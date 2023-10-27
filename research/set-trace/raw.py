da9fe33865fa894fe24211a3d3f90ff8e6015121
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-28 01:08:51 +1100

    verifying else behaviour

--- research/set-trace/raw.py
+++ research/set-trace/raw.py
@@ -1,13 +1,15 @@
 import sys
 
 def trace_function(frame, event, arg):
-    print(f"| {event:10} | {str(arg):>4} |", end=' ')
-    print(f"{frame.f_lineno:>4} |", end=' ')
+    # print(f"| {event:10} | {str(arg):>4} |", end=' ')
+    # print(f"{frame.f_lineno:>4} |", end=' ')
     # print(f"{str(frame.f_locals):<35} |")
+
+    print(frame.f_lineno)
     return trace_function
 
 def main():
-    a()
+    else_block()
 
 def a():
     i = 3
@@ -28,6 +30,16 @@ def b():
         i += 1
 
     print(f"2s: {twos}, 7s: {sevens}")
+
+def else_block():
+    i = 3
+    if i % 2 == 0:
+        print("an even")
+        print("yeah")
+    else:
+        print("an odd number")
+        print("wicked!")
+    print("the end pal")
 
 sys.settrace(trace_function)
 main()
