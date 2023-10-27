5c777b466dd72d663631867f5ad36a5db183237f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-28 00:37:56 +1100

    adding coverage for a while not taken

---
+++ backend/tests/analyse/test_while_not_taken.py
@@ -0,0 +1,17 @@
+def program():
+    i = 3
+    while i < 3:
+        print(i)
+        i += 1
+    print("that's all kids")
+
+from analyse import smart_trace
+from line import Line
+from tree_parser import parse
+
+def test_while_not_taken():
+    root = parse(program)
+    line_mapping = root.map_lines()
+    execution = [Line(2, {}), Line(6, {})]
+    filtered = smart_trace(line_mapping, execution)
+    assert filtered == [Line(2, {}), Line(6, {})]
