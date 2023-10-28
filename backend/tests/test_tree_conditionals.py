29d641e850b4254a85a57fafa4900a166209583b
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-28 15:20:56 +1100

    properly testing method by calling on inheritted class

--- backend/tests/test_tree_conditionals.py
+++ backend/tests/test_tree_conditionals.py
@@ -5,19 +5,15 @@ def program():
     else:
         print("odd")
 
-from tree import Block
 from tree_parser import parse
 
 def test_tree_conditionals():
     root         = parse(program)
     line_mapping = root.map_lines()
+    assert not root.is_conditional()
 
     if_ = line_mapping[3]
     assert if_.is_conditional()
 
     else_ = line_mapping[5]
     assert not else_.is_conditional()
-
-def test_block_basic():
-    block = Block(4, 0)
-    assert not block.is_conditional()
