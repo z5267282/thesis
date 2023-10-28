9e3226ea1e48c4bace7d929c6b40fc3d5f270ba4
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-28 13:56:15 +1100

    adding simple state tests

---
+++ backend/tests/test_state.py
@@ -0,0 +1,16 @@
+from state import State
+
+def test_eq():
+    s1 = State[str]("hello", curr="bye")
+    s2 = State[str]("hello", curr="bye")
+    assert s1 == s2
+
+def test_ne_curr():
+    s1 = State[str]("hello", curr="au revoir")
+    s2 = State[str]("hello", curr="bye")
+    assert s1 != s2
+
+def test_ne_prev():
+    s1 = State[str]("bonjour", curr="bye")
+    s2 = State[str]("hello", curr="bye")
+    assert s1 != s2
