d8fb75a706a3a33b51fbc9c599b09f012d204c76
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-28 15:17:30 +1100

    adding tests for tree conditionals since this is covered in generate

---
+++ backend/tests/test_tree_conditionals.py
@@ -0,0 +1,18 @@
+def program():
+    i = 0
+    if i % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+from tree_parser import parse
+
+def test_tree_conditionals():
+    root         = parse(program)
+    line_mapping = root.map_lines()
+
+    if_ = line_mapping[3]
+    assert if_.is_conditional()
+
+    else_ = line_mapping[5]
+    assert not else_.is_conditional()
