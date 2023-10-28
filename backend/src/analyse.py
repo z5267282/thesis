443552e435df65d051bd3f95e3cd0603826d5fa1
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-28 13:44:00 +1100

    deep copy for curr works

--- backend/src/analyse.py
+++ backend/src/analyse.py
@@ -92,8 +92,8 @@ def trace_while(lines : list[Line], while_ : WhileBlock) -> list[list[Line]]:
         # must make sure we're not on the first line of the whole while
         # this ensures the last check which fails the while is not run
         if line.line_no == while_.start and curr:
-            all_paths.append(curr)
-            curr = []
+            all_paths.append(deepcopy(curr))
+            curr.clear()
 
         curr.append(line)
     
