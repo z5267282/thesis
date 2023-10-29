f03ceefa10b5b29acb90d2fe11fbc45c4544abf7
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-29 22:11:52 +1100

    first line testing done

---
+++ backend/tests/dataframe/test_first_line.py
@@ -0,0 +1,42 @@
+def program():
+    i = 0
+    while i < 3:
+        print(i)
+        i += 1
+
+from collapse import collapse
+from config import ELLIPSE, LEADING_SPACES
+from dataframe import DataFrame
+from helper import get_code_info
+from line import Line
+from state import State
+from tree_parser import parse
+
+def test_first_line():
+    root = parse(program)
+    program_code = get_code_info(program)
+
+    curr = 0
+    counters = []
+    output, evalbox = [], []
+    code, lines, path = collapse([], program_code, root)
+    frame = DataFrame(
+        code, lines,
+        curr, State({}, curr={"i" : 0}), output,
+        path, counters, evalbox
+    )
+    assert frame.to_dict() == {
+        "code"     : [
+            "i = 0",
+            "while i < 3:",
+            "{}{}".format(" " * LEADING_SPACES, ELLIPSE)
+        ],
+        "lines"    : [2, 3, None],
+        "curr"     : curr,
+        "vars"     : [{"name" : "i", "value" : "0", "changed" : True}],
+        "out"      : output,
+        # this should now be changed to not include 0 as the start
+        "path"     : {"start" : 0, "rest" : path[1:]},
+        "counters" : counters,
+        "evalbox"  : evalbox
+    }
