9287b6ccf934cd643f36443475f76e93b4283c2a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-29 22:18:14 +1100

    fixing test case so that the first line is now in the graph

--- backend/tests/dataframe/test_first_line.py
+++ backend/tests/dataframe/test_first_line.py
@@ -16,13 +16,14 @@ def test_first_line():
     root = parse(program)
     program_code = get_code_info(program)
 
-    curr = 0
+    variables = {"i" : 0}
+    curr = 1
     counters = []
-    output, evalbox = [], []
-    code, lines, path = collapse([], program_code, root)
+    output, evalbox = [], ["while 1 < 3"]
+    code, lines, path = collapse([Line(2, {}), Line(3, {})], program_code, root)
     frame = DataFrame(
         code, lines,
-        curr, State({}, curr={"i" : 0}), output,
+        curr, State(variables, curr=variables), output,
         path, counters, evalbox
     )
     assert frame.to_dict() == {
@@ -33,7 +34,7 @@ def test_first_line():
         ],
         "lines"    : [2, 3, None],
         "curr"     : curr,
-        "vars"     : [{"name" : "i", "value" : "0", "changed" : True}],
+        "vars"     : [{"name" : "i", "value" : "0", "changed" : False}],
         "out"      : output,
         # this should now be changed to not include 0 as the start
         "path"     : {"start" : 0, "rest" : path[1:]},
