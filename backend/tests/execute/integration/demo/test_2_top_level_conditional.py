23a9f790f35728a8b2dc7b3600558a57d53ee904
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-30 16:24:57 +1100

    adding second demo test

---
+++ backend/tests/execute/integration/demo/test_2_top_level_conditional.py
@@ -0,0 +1,218 @@
+def program():
+    i = 0
+    print(f"i is {i}")
+    if i % 7 == 0:
+        print("a special number!")
+        print(":D")
+    else:
+        print("meh")
+    print("that's all folks")
+
+from dataframe import DataFrame
+from main import main
+
+def test_2_top_level_conditional():
+    assert DataFrame.to_dicts(main()) == [
+        {
+            "code": [
+                "i = 0",
+                "print(f\"i is {i}\")",
+                "if i % 7 == 0:",
+                "    \u00b7\u00b7\u00b7",
+                "else:",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"that's all folks\")"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3",
+                "",
+                "6",
+                "",
+                "8"
+            ],
+            "curr": None,
+            "vars": [],
+            "out": [],
+            "path": {
+                "start": 0,
+                "rest": []
+            },
+            "counters": [],
+            "evalbox": []
+        },
+        {
+            "code": [
+                "i = 0",
+                "print(f\"i is {i}\")",
+                "if i % 7 == 0:",
+                "    \u00b7\u00b7\u00b7",
+                "else:",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"that's all folks\")"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3",
+                "",
+                "6",
+                "",
+                "8"
+            ],
+            "curr": 1,
+            "vars": [
+                {
+                    "name": "i",
+                    "value": "0",
+                    "changed": True
+                }
+            ],
+            "out": [
+                "i is 0\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": [
+                    1
+                ]
+            },
+            "counters": [],
+            "evalbox": []
+        },
+        {
+            "code": [
+                "i = 0",
+                "print(f\"i is {i}\")",
+                "if i % 7 == 0:",
+                "    \u00b7\u00b7\u00b7",
+                "else:",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"that's all folks\")"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3",
+                "",
+                "6",
+                "",
+                "8"
+            ],
+            "curr": 2,
+            "vars": [
+                {
+                    "name": "i",
+                    "value": "0",
+                    "changed": False
+                }
+            ],
+            "out": [
+                "i is 0\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": [
+                    1,
+                    2
+                ]
+            },
+            "counters": [],
+            "evalbox": [
+                "0 % 7 == 0"
+            ]
+        },
+        {
+            "code": [
+                "i = 0",
+                "print(f\"i is {i}\")",
+                "if i % 7 == 0:",
+                "    print(\"a special number!\")",
+                "    print(\":D\")",
+                "else:",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"that's all folks\")"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "",
+                "8"
+            ],
+            "curr": 4,
+            "vars": [
+                {
+                    "name": "i",
+                    "value": "0",
+                    "changed": False
+                }
+            ],
+            "out": [
+                "i is 0\n",
+                "a special number!\n",
+                ":D\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": [
+                    1,
+                    2,
+                    4
+                ]
+            },
+            "counters": [],
+            "evalbox": []
+        },
+        {
+            "code": [
+                "i = 0",
+                "print(f\"i is {i}\")",
+                "if i % 7 == 0:",
+                "    print(\"a special number!\")",
+                "    print(\":D\")",
+                "else:",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"that's all folks\")"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "",
+                "8"
+            ],
+            "curr": 7,
+            "vars": [
+                {
+                    "name": "i",
+                    "value": "0",
+                    "changed": False
+                }
+            ],
+            "out": [
+                "i is 0\n",
+                "a special number!\n",
+                ":D\n",
+                "that's all folks\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": [
+                    1,
+                    2,
+                    4,
+                    7
+                ]
+            },
+            "counters": [],
+            "evalbox": []
+        }
+    ]
