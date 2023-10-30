8cd995baeb258183375574e8c0ae1d7d9b9acca9
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-30 13:26:58 +1100

    first integration test works

--- backend/tests/execute/integration/demo/test_1_top_level.py
+++ backend/tests/execute/integration/demo/test_1_top_level.py
@@ -1,2 +1,68 @@
+def program():
+    i = 42
+    j = 69
+    print(f"{i} + {j} = {i + j}")
+
+from dataframe import DataFrame
+from main import main
+
 def test_1_top_level():
-    pass
+    assert DataFrame.to_dicts(main()) == [
+        {
+            "code": [
+                "i = 42",
+                "j = 69",
+                "print(f\"{i} + {j} = {i + j}\")"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3"
+            ],
+            "curr": None,
+            "vars": [],
+            "out": [],
+            "path": {
+                "start": 0,
+                "rest": []
+            },
+            "counters": [],
+            "evalbox": []
+        },
+        {
+            "code": [
+                "i = 42",
+                "j = 69",
+                "print(f\"{i} + {j} = {i + j}\")"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3"
+            ],
+            "curr": 2,
+            "vars": [
+                {
+                    "name": "i",
+                    "value": "42",
+                    "changed": False
+                },
+                {
+                    "name": "j",
+                    "value": "69",
+                    "changed": False
+                }
+            ],
+            "out": [
+                "42 + 69 = 111\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": [
+                    2
+                ]
+            },
+            "counters": [],
+            "evalbox": []
+        }
+    ]
