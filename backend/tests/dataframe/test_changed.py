2c6b307fb8694e71b957b3980dbb8f8d901f9b9b
Sunny Chen <z5267282@student.unsw.edu.au>
2023-10-31 14:15:20 +1100

    need to think about current

--- backend/tests/dataframe/test_changed.py
+++ backend/tests/dataframe/test_changed.py
@@ -14,3 +14,14 @@ def test_changed_from_prev():
     curr = State(curr_vars, curr={"i" : 42})
     frame = DataFrame([], [], None, curr, prev, [], [], [], [])
     assert frame.is_changed("i")
+
+def introduced_from_line():
+    old = {"i" : 0}
+    prev = State(old, curr=old)
+    curr = State(old, curr={"i" : 0, "j" : 42})
+    frame = DataFrame([], [], None, curr, prev, [], [], [], [])
+    assert frame.is_changed("j")
+    # unchanged variable should stay the same
+    assert not frame.is_changed("i")
+
+# TODO : prev vars tests
