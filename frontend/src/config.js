c7e5eb039e5c6480f2a5e563d424d951dd978eff
Sunny Chen <z5267282@student.unsw.edu.au>
2023-11-17 01:54:26 +1100

    let's go

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -7,6 +7,7 @@ export const TABS = { TRACE : "trace", UPLOAD : "upload" };
 
 export const TRACE_GRAPH = {
   width  : 30,
+  // from arrow tangent
   degree : Math.PI / 6,
   // of arrow head
   length : 15
@@ -16,10 +17,8 @@ export const EDITOR_TAB_SPACES = 4;
 
 export const COUNTER_COLOURS = ["#649FFF", "#317FFF"]
 
-const setServer = () => {
-  return process.env.REACT_APP_HOST === "REMOTE" ?
+export const SERVER =
+  (process.env.REACT_APP_HOST === "REMOTE") ?
     "https://z5267282.pythonanywhere.com"
   :
     "http://127.0.0.1:5000";
-};
-export const SERVER = setServer();
