331ff3120262ce1060c9d3bb7124d4ee92c5b1c8
Sunny Chen <z5267282@student.unsw.edu.au>
2023-11-28 22:24:54 +1100

    tests broken

---
+++ backend/tests/function_calls/tree_parse/test_nested.py
@@ -0,0 +1,46 @@
+def program():
+    def a():
+        def b():
+            print("a")
+        
+        b()
+        print("a")
+    
+    print("start")
+    a()
+
+from tree_parse import parse
+
+def test_nested():
+    root = parse(program)
+    assert root.to_dict() == {
+        "BodyBlock": {
+            "start": 2, "end": 10, "body": [
+                {
+                    "FunctionBlock": {
+                        "start": 2, "end": 9, "body": [
+                            {
+                                "FunctionBlock": {
+                                    "start": 3, "end": 5, "body": [
+                                        {
+                                            "CodeBlock": {
+                                                "start": 4, "end": 5
+                                            }
+                                        }
+                                    ]
+                                },
+                                "CodeBlock": {
+                                    "start": 6, "end": 7,
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    "CodeBlock": {
+                        "start": 9, "end": 10
+                    }
+                }
+            ]
+        }
+    }
