62a2583d6f19e78f396b0fc49ead44026f3f5f4c
Sunny Chen <z5267282@student.unsw.edu.au>
2023-11-30 01:53:44 +1100

    longer named function

--- backend/src/execute.py
+++ backend/src/execute.py
@@ -1,7 +1,9 @@
+from collections import OrderedDict
 from copy import deepcopy
 from io import StringIO
 import sys
 
+from helper import get_code_info, get_stripped_line
 from last import Last
 from line import Line
 from state import State
@@ -13,12 +15,15 @@ def trace_program(program : Callable):
     Return a list of Line objects representing the program's raw execution
     path."""
     buffer  : StringIO = StringIO()
-    lines   : list[Line] = []
+    lines   : list[list[Line]] = []
+    curr    : list[Line] = []
+    code    : OrderedDict[int, str] = get_code_info(program)
     output  : list[str] = []
     printed : State[str] = State("", curr="")
     last    : Last = Last()
+
     def wrapper(frame : FrameType, event : str, arg : Any):
-        trace_line(frame, event, arg, lines, output, buffer, printed, last)
+        trace_line(frame, event, arg, lines, curr, code, output, buffer, printed, last)
         return wrapper
 
     sys.stdout = buffer
@@ -30,10 +35,14 @@ def trace_program(program : Callable):
 
 def trace_line(
     frame : FrameType, event : str, _ : Any, lines : list[list[Line]],
-    curr : list[Line],
+    curr : list[Line], code : OrderedDict[int, str],
     output : list[str], buffer : StringIO, printed : State, last : Last
 ):
     if not event in set("call", "line", "return"):
+        return
+    
+    # skip over the running of function definitions
+    if event == "line" and get_stripped_line(code[frame.f_lineno]).startswith("def"):
         return
 
     # state related steps
@@ -47,18 +56,15 @@ def trace_line(
 
     # manage previous state
     # note a "previous" state needs to exist (ie. line > starting)
-    if lines:
-        lines[-1].output.extend(output)
+    if curr:
+        curr[-1].output.extend(output)
     
-    curr.append(
-        Line(frame.f_lineno, event, variables=variables)
-    )
-    match event:
-        case "line":
-            lines.append(Line())
-        case "return":
-            last.variables.update(frame.f_locals)
-            last.output.extend(output)
+    line : Line = Line(frame.f_lineno, event, variables=variables)
+    curr.append(line)
+    if event == "return":
+        line.output.extend(output)
+        lines.append(deepcopy(curr))
+        curr.clear()
 
 def string_diff(prev : str, curr : str):
     """Given that prev is a prefix of curr, obtain the difference:
