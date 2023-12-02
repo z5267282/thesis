90e1ca489ce84cd53ac729ea51005dcccbf6f9e8
Sunny Chen <z5267282@student.unsw.edu.au>
2023-12-02 19:09:07 +1100

    debugging by printing out json file

--- backend/tests/function_calls/generate/test_simple.py
+++ backend/tests/function_calls/generate/test_simple.py
@@ -5,8 +5,14 @@ def program():
     print("start")
     a()
 
+from dataframe import DataFrame
 from generate import generate_dataframes
 
 def test_simple():
     frames = generate_dataframes(program)
+
+    from json import dump
+    with open("/tmp/a.json", "w") as f:
+        dump(DataFrame.to_dicts(frames), f, indent=2)
+
     assert frames == []
