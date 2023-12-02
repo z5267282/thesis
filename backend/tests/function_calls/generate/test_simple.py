c503e36e920084855c0a10321a7d97d660c8d1c7
Sunny Chen <z5267282@student.unsw.edu.au>
2023-12-02 22:38:52 +1100

    one test for function call dataframe

--- backend/tests/function_calls/generate/test_simple.py
+++ backend/tests/function_calls/generate/test_simple.py
@@ -10,9 +10,176 @@ from generate import generate_dataframes
 
 def test_simple():
     frames = generate_dataframes(program)
-
-    from json import dump
-    with open("/tmp/a.json", "w") as f:
-        dump(DataFrame.to_dicts(frames), f, indent=2)
-
-    assert frames == []
+    assert DataFrame.to_dicts(frames) == [
+        {
+            "code": [
+                "def a():",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"start\")",
+                "a()"
+            ],
+            "lines": [
+                "1",
+                "",
+                "4",
+                "5"
+            ],
+            "curr": None,
+            "vars": [],
+            "out": [],
+            "path": None,
+            "counters": [],
+            "evalbox": [],
+            "call": None
+        },
+        {
+            "code": [
+                "def a():",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"start\")",
+                "a()"
+            ],
+            "lines": [
+                "1",
+                "",
+                "4",
+                "5"
+            ],
+            "curr": 3,
+            "vars": [],
+            "out": [],
+            "path": {
+                "start": 2,
+                "rest": [
+                    3
+                ]
+            },
+            "counters": [],
+            "evalbox": [],
+            "call": None
+        },
+        {
+            "code": [
+                "def a():",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"start\")",
+                "a()"
+            ],
+            "lines": [
+                "1",
+                "",
+                "4",
+                "5"
+            ],
+            "curr": 0,
+            "vars": [],
+            "out": [
+                "start\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": []
+            },
+            "counters": [],
+            "evalbox": [],
+            "call": {
+                "entry": 3,
+                "target": 0,
+                "return": False
+            }
+        },
+        {
+            "code": [
+                "def a():",
+                "    print(\"hi\")",
+                "",
+                "print(\"start\")",
+                "a()"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5"
+            ],
+            "curr": 1,
+            "vars": [],
+            "out": [
+                "start\n",
+                "hi\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": [
+                    1
+                ]
+            },
+            "counters": [],
+            "evalbox": [],
+            "call": {
+                "entry": 4,
+                "target": 0,
+                "return": False
+            }
+        },
+        {
+            "code": [
+                "def a():",
+                "    print(\"hi\")",
+                "",
+                "print(\"start\")",
+                "a()"
+            ],
+            "lines": [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5"
+            ],
+            "curr": 1,
+            "vars": [],
+            "out": [
+                "start\n",
+                "hi\n"
+            ],
+            "path": {
+                "start": 0,
+                "rest": [
+                    1
+                ]
+            },
+            "counters": [],
+            "evalbox": [],
+            "call": {
+                "entry": 4,
+                "target": 0,
+                "return": True
+            }
+        },
+        {
+            "code": [
+                "def a():",
+                "    \u00b7\u00b7\u00b7",
+                "print(\"start\")",
+                "a()"
+            ],
+            "lines": [
+                "1",
+                "",
+                "4",
+                "5"
+            ],
+            "curr": None,
+            "vars": [],
+            "out": [
+                "start\n",
+                "hi\n"
+            ],
+            "path": None,
+            "counters": [],
+            "evalbox": [],
+            "call": None
+        }
+    ]
