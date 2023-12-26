69905c76b95b86542c264a1df43d0063344b8177
Sunny Chen <z5267282@student.unsw.edu.au>
2023-12-26 21:12:04 +1000

    evaluate type hints

--- backend/src/evaluate.py
+++ backend/src/evaluate.py
@@ -1,8 +1,8 @@
 from typing import Any
 
-def evaluate(expression : str, variables : dict[str, Any]):
+def evaluate(expression : str, variables : dict[str, Any]) -> str:
     """A simple string replacement algorithm; will not evaluate expressions""" 
-    result = expression
+    result : str = expression
     for name, value in variables.items():
         result = result.replace(name, str(value))
     return result
