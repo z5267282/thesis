79f582b357148c5f2d5369196869e7916a8aeb7f
Sunny Chen <z5267282@student.unsw.edu.au>
2023-12-26 21:04:05 +1000

    main type hints

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,10 @@
 import importlib
 
+from dataframe import DataFrame
 from generate import generate_dataframes
 import program
 
-def main():
+def main() -> list[DataFrame]:
     """Execute a program and generate a list of DataFrames representing
     execution states."""
     importlib.reload(program)
