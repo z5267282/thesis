b54e62627b9be49fdb632a8f8b17e65b54ca14c7
Sunny Chen <z5267282@student.unsw.edu.au>
2023-12-26 21:03:08 +1000

    stack type hints

--- backend/src/stack.py
+++ backend/src/stack.py
@@ -1,13 +1,13 @@
-from typing import Generic, Type, TypeVar
+from typing import Generic, Iterator, Type, TypeVar
 
 T = TypeVar("T")
 
 class Stack(Generic[T]):
     """A LIFO data structure"""
-    def __init__(self):
+    def __init__(self) -> None:
         self.items : list[Type[T]] = []
     
-    def __str__(self):
+    def __str__(self) -> str:
         """Print items from top to bottom"""
         return "\n".join(
             f"{i} : {block}" 
@@ -15,29 +15,29 @@ class Stack(Generic[T]):
                 for i, block in enumerate(reversed(self.items), start=1)
         )
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.items)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[T]:
         return iter(self.items)
     
-    def empty(self):
+    def empty(self) -> bool:
         return len(self) == 0
     
-    def peek(self):
+    def peek(self) -> T:
         """Retrive the top item without removing it.
         Method according to the documentation here:
         https://docs.python.org/3/library/collections.html#collections.deque"""
         return self.items[-1]
 
-    def pop(self):
+    def pop(self) -> T:
         """Remove the top item and return it"""
         return self.items.pop()
     
-    def pop_peek(self):
+    def pop_peek(self) -> T:
         """Remove the top item and return the new top"""
         self.pop()
         return self.peek()
     
-    def push(self, item : Type[T]):
+    def push(self, item : Type[T]) -> None:
         self.items.append(item)
