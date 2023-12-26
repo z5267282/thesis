35bce0a5f81087445b2e982676fb39afef206460
Sunny Chen <z5267282@student.unsw.edu.au>
2023-12-26 21:05:09 +1000

    line type hints

--- backend/src/line.py
+++ backend/src/line.py
@@ -6,29 +6,29 @@ from tree import WhileBlock
 
 class Line:
     """A dataclass to store line information"""
-    def __init__(self, line_no : int, variables : dict[str, Any]=None):
+    def __init__(self, line_no : int, variables : dict[str, Any]=None) -> None:
         self.line_no   : int = line_no
         self.output    : list[str] = []
         self.variables : dict[str, Any] = {} if variables is None else variables
         # counters are stored from least indented to most indented
         self.counters  : list[Counter] = []
     
-    def __repr__(self): # pragma: no cover
+    def __repr__(self) -> str: # pragma: no cover
         """Return a simple representation to assist with debugging asserts."""
         return str(self.line_no)
     
-    def __eq__(self, other : "Line"):
+    def __eq__(self, other : "Line") -> bool:
         return self.line_no == other.line_no
 
-    def __ne__(self, other : "Line"):
+    def __ne__(self, other : "Line") -> bool:
         return not self == other
     
     @staticmethod
-    def display_lines(lines : list["Line"]): # pragma: no cover
+    def display_lines(lines : list["Line"]) -> str: # pragma: no cover
         """A debugging method to format a list of lines for printing"""
         return ", ".join(str(line.line_no) for line in lines)
     
-    def long_str(self):
+    def long_str(self) -> str:
         delim = ",\n{}".format(" " * 8)
         counters : str = """
         {}
@@ -45,17 +45,19 @@ class Line:
     vars : {{{dict_to_str(self.variables)}}}
     counters : [{counters}]"""
 
-    def fix_lag(self, output : list[str], variables : dict[str, Any]):
+    def fix_lag(self, output : list[str], variables : dict[str, Any]) -> None:
         """Fix a line so that it has the right state.
         This is based on the limitaton of sys.settrace which runs lines
         as they are entered, not as they are left."""
         self.output.extend(output)
         self.variables.update(variables)
     
-    def add_counter(self, iteration : int, total : int, while_ : WhileBlock):
+    def add_counter(
+        self, iteration : int, total : int, while_ : WhileBlock
+    ) -> None:
         """Add a counter of an increased depth"""
         self.counters.append(Counter(iteration, total, while_))
     
-    def range_filter_counters(self, filtered : OrderedDict[int, bool]):
+    def range_filter_counters(self, filtered : OrderedDict[int, bool]) -> None:
         for counter in self.counters:
             counter.find_filtered_range(filtered, self.line_no)
