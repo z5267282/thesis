e0f4a87280c43e3fa647f3a5cfe22ab9b1c0468a
Sunny Chen <z5267282@student.unsw.edu.au>
2023-12-26 21:10:29 +1000

    generate tests

--- backend/src/generate.py
+++ backend/src/generate.py
@@ -15,7 +15,7 @@ from state import State
 from tree import Block, BodyBlock
 from tree_parser import parse
 
-def generate_dataframes(program : Callable):
+def generate_dataframes(program : Callable) -> list[DataFrame]:
     """Given a program, intelligently execute it and generate the necessary
     DataFrames to display execution"""
     all_lines, last = trace_program(program)
@@ -31,11 +31,11 @@ def generate_dataframes(program : Callable):
 def construct_dataframes(
     program_code : OrderedDict[int, str], line_graphs : list[list[Line]],
     root : BodyBlock, line_mapping : dict[int, Type[Block]], last : Line
-):
+) -> list[DataFrame]:
     first_frame : DataFrame = generate_edge_dataframe(
         program_code, root, {}, []
     )
-    prev_vars   : dict[str, Any] = first_frame.variables.curr
+    prev_vars      : dict[str, Any] = first_frame.variables.curr
     program_frames : list[DataFrame] = []
     for line_graph in line_graphs:
         dataframe : DataFrame = generate_dataframe(
@@ -53,7 +53,7 @@ def construct_dataframes(
 def generate_edge_dataframe(
     program_code : OrderedDict[int, str], root : BodyBlock,
     variables : dict[str, Any], output : list[str]
-):
+) -> DataFrame:
     """Construct a DataFrame that is either before or after the program's
     execution.
     Hence, these frames only have program state and no execution graph."""
@@ -67,7 +67,7 @@ def generate_dataframe(
     line_graph : list[Line], program_code : OrderedDict[int, str],
     root : BodyBlock, line_mapping : dict[int, Type[Block]],
     prev_vars : dict[str, Any]
-):
+) -> DataFrame:
     code, lines, path = collapse(line_graph, program_code, root)
     curr : Line = line_graph[-1]
 
@@ -84,7 +84,7 @@ def generate_dataframe(
         curr.output, path, curr.counters, evalbox
     )
 
-def generate_evalbox(line : str, variables : dict[str, Any]):
+def generate_evalbox(line : str, variables : dict[str, Any]) -> str:
     """Given a line with a conditional expression, expand it from a mapping
     of variable values."""
     raw_line        : str = get_stripped_line(line)
@@ -92,7 +92,7 @@ def generate_evalbox(line : str, variables : dict[str, Any]):
     expression      : str = re.sub(r":[^:]*$", "", no_control_flow)
     return evaluate(expression, variables)
 
-def adjust_lines(lines):
+def adjust_lines(lines) -> list[str]:
     """Adjust line numbers so they are displayed correctly.
     Collapsed lines should be represented by an empty string.
     Line numbers should start from 1."""
